4154f54005da142af7c227d4e30ed371a0701596
Frank Corso <fpcorso@gmail.com>
2014-11-19 15:33:45 -0500

    Created Alert Class
    Created the MlwQmnAlertManager class. This class stores alerts in array
    and then displays the alerts.

--- includes/mlw_alerts.php
+++ includes/mlw_alerts.php
@@ -1,4 +1,30 @@
 <?php
-
-
+class MlwQmnAlertManager {
+	
+	public $alerts = array();
+	
+	public function newAlert($message, $type)
+	{
+		$this->alerts[] = array( 'message' => $message, 'type' => $type );
+	}
+	
+	public function showAlerts()
+	{
+		$alert_list = "";
+		foreach ($this->alerts as $alert)
+		{
+			if ($alert['type'] == "success")
+			{
+				$alert_list .= "<div id=\"message\" class=\"updated below-h2\"><p><strong>Success! </strong>".$alert["message"]."</p></div>";
+			}
+			if ($alert['type'] == "error")
+			{
+				$alert_list .= "<div id=\"message\" class=\"error below-h2\"><p><strong>Error! </strong>".$alert["message"]."</p></div>";
+			}
+		}
+		echo $alert_list;
+	}
+	
+}
+$mlwQmnAlertManager = new MlwQmnAlertManager();
 ?>
