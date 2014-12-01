7cb729f5f4464241f396b6f4c3c08746c45406ed
Frank Corso <fpcorso@gmail.com>
2014-11-30 21:00:35 -0500

    Prepare Main File For Release

--- mlw_quizmaster2.php
+++ mlw_quizmaster2.php
@@ -3,7 +3,7 @@
 /*
 Plugin Name: Quiz Master Next
 Description: Use this plugin to add multiple quizzes, tests, or surveys to your website.
-Version: 3.5.2
+Version: 3.6.1
 Author: Frank Corso
 Author URI: http://www.mylocalwebstop.com/
 Plugin URI: http://www.mylocalwebstop.com/
