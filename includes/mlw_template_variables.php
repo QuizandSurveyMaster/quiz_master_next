6b354fe699402693046329400d241cd24385f6ca
Frank Corso <fpcorso@gmail.com>
2014-11-30 20:16:13 -0500

    Updated Filter Name, New Variable, New Filter Hook
    Added the functions for the new mlw_qmn_template_variable_quiz_page filter. Updated the name of the other filters. Added variable for display name.

--- includes/mlw_template_variables.php
+++ includes/mlw_template_variables.php
@@ -23,87 +23,97 @@ $mlw_qmn_result_array = array(
 		);
 
 */
-add_filter('mlw_qmn_text_variable', 'mlw_qmn_variable_point_score',10,2);
-add_filter('mlw_qmn_text_variable', 'mlw_qmn_variable_average_point',10,2);
-add_filter('mlw_qmn_text_variable', 'mlw_qmn_variable_amount_correct',10,2);
-add_filter('mlw_qmn_text_variable', 'mlw_qmn_variable_total_questions',10,2);
-add_filter('mlw_qmn_text_variable', 'mlw_qmn_variable_correct_score',10,2);
-add_filter('mlw_qmn_text_variable', 'mlw_qmn_variable_quiz_name',10,2);
-add_filter('mlw_qmn_text_variable', 'mlw_qmn_variable_user_name',10,2);
-add_filter('mlw_qmn_text_variable', 'mlw_qmn_variable_user_business',10,2);
-add_filter('mlw_qmn_text_variable', 'mlw_qmn_variable_user_phone',10,2);
-add_filter('mlw_qmn_text_variable', 'mlw_qmn_variable_user_email',10,2);
-add_filter('mlw_qmn_text_variable', 'mlw_qmn_variable_question_answers',10,2);
-add_filter('mlw_qmn_text_variable', 'mlw_qmn_variable_comments',10,2);
-add_filter('mlw_qmn_text_variable', 'mlw_qmn_variable_timer',10,2);
-add_filter('mlw_qmn_text_variable', 'mlw_qmn_variable_date',10,2);
-function mlw_qmn_variable_point_score($content, $mlw_results_array)
+add_filter('mlw_qmn_template_variable_results_page', 'mlw_qmn_variable_point_score',10,2);
+add_filter('mlw_qmn_template_variable_results_page', 'mlw_qmn_variable_average_point',10,2);
+add_filter('mlw_qmn_template_variable_results_page', 'mlw_qmn_variable_amount_correct',10,2);
+add_filter('mlw_qmn_template_variable_results_page', 'mlw_qmn_variable_total_questions',10,2);
+add_filter('mlw_qmn_template_variable_results_page', 'mlw_qmn_variable_correct_score',10,2);
+add_filter('mlw_qmn_template_variable_results_page', 'mlw_qmn_variable_quiz_name',10,2);
+add_filter('mlw_qmn_template_variable_results_page', 'mlw_qmn_variable_user_name',10,2);
+add_filter('mlw_qmn_template_variable_results_page', 'mlw_qmn_variable_user_business',10,2);
+add_filter('mlw_qmn_template_variable_results_page', 'mlw_qmn_variable_user_phone',10,2);
+add_filter('mlw_qmn_template_variable_results_page', 'mlw_qmn_variable_user_email',10,2);
+add_filter('mlw_qmn_template_variable_results_page', 'mlw_qmn_variable_question_answers',10,2);
+add_filter('mlw_qmn_template_variable_results_page', 'mlw_qmn_variable_comments',10,2);
+add_filter('mlw_qmn_template_variable_results_page', 'mlw_qmn_variable_timer',10,2);
+add_filter('mlw_qmn_template_variable_results_page', 'mlw_qmn_variable_date',10,2);
+
+add_filter('mlw_qmn_template_variable_quiz_page', 'mlw_qmn_variable_quiz_name',10,2);
+add_filter('mlw_qmn_template_variable_quiz_page', 'mlw_qmn_variable_date',10,2);
+add_filter('mlw_qmn_template_variable_quiz_page', 'mlw_qmn_variable_current_user',10,2);
+function mlw_qmn_variable_point_score($content, $mlw_quiz_array)
 {
-	$content = str_replace( "%POINT_SCORE%" , $mlw_results_array["total_point"], $content);
+	$content = str_replace( "%POINT_SCORE%" , $mlw_quiz_array["total_point"], $content);
 	return $content;
 }
-function mlw_qmn_variable_average_point($content, $mlw_results_array)
+function mlw_qmn_variable_average_point($content, $mlw_quiz_array)
 {
-	$mlw_average_points = $mlw_results_array["total_point"]/$mlw_results_array["total_questions"];
+	$mlw_average_points = $mlw_quiz_array["total_point"]/$mlw_quiz_array["total_questions"];
 	$content = str_replace( "%AVERAGE_POINT%" , $mlw_average_points, $content);
 	return $content;
 }
-function mlw_qmn_variable_amount_correct($content, $mlw_results_array)
+function mlw_qmn_variable_amount_correct($content, $mlw_quiz_array)
 {
-	$content = str_replace( "%AMOUNT_CORRECT%" , $mlw_results_array["total_correct"], $content);
+	$content = str_replace( "%AMOUNT_CORRECT%" , $mlw_quiz_array["total_correct"], $content);
 	return $content;
 }
-function mlw_qmn_variable_total_questions($content, $mlw_results_array)
+function mlw_qmn_variable_total_questions($content, $mlw_quiz_array)
 {
-	$content = str_replace( "%TOTAL_QUESTIONS%" , $mlw_results_array["total_questions"], $content);
+	$content = str_replace( "%TOTAL_QUESTIONS%" , $mlw_quiz_array["total_questions"], $content);
 	return $content;
 }
-function mlw_qmn_variable_correct_score($content, $mlw_results_array)
+function mlw_qmn_variable_correct_score($content, $mlw_quiz_array)
 {
-	$content = str_replace( "%CORRECT_SCORE%" , $mlw_results_array["total_score"], $content);
+	$content = str_replace( "%CORRECT_SCORE%" , $mlw_quiz_array["total_score"], $content);
 	return $content;
 }
-function mlw_qmn_variable_quiz_name($content, $mlw_results_array)
+function mlw_qmn_variable_quiz_name($content, $mlw_quiz_array)
 {
-	$content = str_replace( "%QUIZ_NAME%" , $mlw_results_array["quiz_name"], $content);
+	$content = str_replace( "%QUIZ_NAME%" , $mlw_quiz_array["quiz_name"], $content);
 	return $content;
 }
-function mlw_qmn_variable_user_name($content, $mlw_results_array)
+function mlw_qmn_variable_user_name($content, $mlw_quiz_array)
 {
-	$content = str_replace( "%USER_NAME%" , $mlw_results_array["user_name"], $content);
+	$content = str_replace( "%USER_NAME%" , $mlw_quiz_array["user_name"], $content);
 	return $content;
 }
-function mlw_qmn_variable_user_business($content, $mlw_results_array)
+function mlw_qmn_variable_current_user($content, $mlw_quiz_array)
 {
-	$content = str_replace( "%USER_BUSINESS%" , $mlw_results_array["user_business"], $content);
+	$current_user = wp_get_current_user();
+	$content = str_replace( "%USER_NAME%" , $current_user->display_name, $content);
 	return $content;
 }
-function mlw_qmn_variable_user_phone($content, $mlw_results_array)
+function mlw_qmn_variable_user_business($content, $mlw_quiz_array)
 {
-	$content = str_replace( "%USER_PHONE%" , $mlw_results_array["user_phone"], $content);
+	$content = str_replace( "%USER_BUSINESS%" , $mlw_quiz_array["user_business"], $content);
 	return $content;
 }
-function mlw_qmn_variable_user_email($content, $mlw_results_array)
+function mlw_qmn_variable_user_phone($content, $mlw_quiz_array)
 {
-	$content = str_replace( "%USER_EMAIL%" , $mlw_results_array["user_email"], $content);
+	$content = str_replace( "%USER_PHONE%" , $mlw_quiz_array["user_phone"], $content);
 	return $content;
 }
-function mlw_qmn_variable_question_answers($content, $mlw_results_array)
+function mlw_qmn_variable_user_email($content, $mlw_quiz_array)
+{
+	$content = str_replace( "%USER_EMAIL%" , $mlw_quiz_array["user_email"], $content);
+	return $content;
+}
+function mlw_qmn_variable_question_answers($content, $mlw_quiz_array)
 {
 	$content = str_replace( "%QUESTIONS_ANSWERS%" , $mlw_question_answers, $content);
 	return $content;
 }
-function mlw_qmn_variable_comments($content, $mlw_results_array)
+function mlw_qmn_variable_comments($content, $mlw_quiz_array)
 {
 	$content = str_replace( "%COMMENT_SECTION%" , $mlw_qm_quiz_comments, $content);
 	return $content;
 }
-function mlw_qmn_variable_timer($content, $mlw_results_array)
+function mlw_qmn_variable_timer($content, $mlw_quiz_array)
 {
 	$content = str_replace( "%TIMER%" , $mlw_qmn_timer, $content);
 	return $content;
 }
-function mlw_qmn_variable_date($content, $mlw_results_array)
+function mlw_qmn_variable_date($content, $mlw_quiz_array)
 {
 	$content = str_replace( "%CURRENT_DATE%" , date("F jS Y"), $content);
 	return $content;
