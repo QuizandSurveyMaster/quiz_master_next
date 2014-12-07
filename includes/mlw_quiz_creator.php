e31f2e84bd821f5d3c50a06b4f223e8f03bdb42d
Frank Corso <fpcorso@gmail.com>
2014-12-07 08:06:58 -0500

    Added Quiz ID Variable

--- includes/mlw_quiz_creator.php
+++ includes/mlw_quiz_creator.php
@@ -1,11 +1,34 @@
 <?php
-
+/**
+ * QMN Quiz Creator Class
+ *
+ * This class handles quiz creation, update, and deletion from the admin panel
+ *
+ * The Quiz Creator class handles all the quiz management functions that is done from the admin panel
+ *
+ * @since 3.7.1
+ */
 class QMNQuizCreator
 {
+	/**
+	 * QMN ID of quiz
+	 *
+	 * @var object
+	 * @since 3.7.1
+	 */
+	private $quiz_id;
 	
+	/**
+	 * If the quiz ID is set, store it as the class quiz ID
+	 *
+	 * @since 3.7.1
+	 */
 	public function __construct()
 	{
-	
+		if (isset($_GET["quiz_id"]))
+		{
+			$this->quiz_id = intval($_GET["quiz_id"]);
+		}
 	}
 }
 ?>
