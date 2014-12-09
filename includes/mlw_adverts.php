de79fc8c15b20a4adf99d7932a77243441ac8c58
Frank Corso <fpcorso@gmail.com>
2014-12-09 13:33:20 -0500

    Added New Survey URL

--- includes/mlw_adverts.php
+++ includes/mlw_adverts.php
@@ -6,7 +6,7 @@ function mlw_qmn_show_adverts()
 	$mlw_advert_text = "";
 	if ( get_option('mlw_advert_shows') == 'true' )
 	{
-		$mlw_random_int = rand(0, 8);
+		$mlw_random_int = rand(0, 7);
 		switch ($mlw_random_int) {
 			case 0:
 				$mlw_advert_text = "Need support or features? Check out our Premium Support options! Visit our <a href=\"http://mylocalwebstop.com/shop/\">WordPress Store</a> for details!";
@@ -21,19 +21,16 @@ function mlw_qmn_show_adverts()
 				$mlw_advert_text = "Would you like to support this plugin but do not need or want premium support? Please consider our inexpensive 'Advertisements Be Gone' add-on which will get rid of these ads. Visit our <a href=\"http://mylocalwebstop.com/shop/\">Plugin Add-On Store</a> for details!";
 				break;
 			case 4:
-				$mlw_advert_text = "Need to be able to export the results of your quizzes? Be sure to check out our new Export Results add-on in our <a href=\"http://mylocalwebstop.com/shop/\">WordPress Store</a>!";
-				break;
-			case 5:
 				$mlw_advert_text = "Need help keeping your plugins, themes, and WordPress up to date? Want around the clock security monitoring and off-site back-ups? How about WordPress training videos, a monthly status report, and support/consultation? Check out our <a href=\"http://mylocalwebstop.com/wordpress-maintenance-services/\">WordPress Maintenance Services</a> for more details!";
 				break;
-			case 6:
+			case 5:
 				$mlw_advert_text = "Setting up a new site? Let us take care of the set-up so you back to running your business. Check out our <a href=\"http://mylocalwebstop.com/shop/\">WordPress Store</a> for more details!";
 				break;
-			case 7:
-				$mlw_advert_text = "Need a page so your users can see their results from all the quizzes they have taken? Try our new User Dashboard add-on. Check out our <a href=\"http://mylocalwebstop.com/shop/\">WordPress Store</a> for more details!";
-				break;
-			case 8:
+			case 6:
 				$mlw_advert_text = "Our new Popular Add-On Pack is now only $45 for 6 add-ons! Now is the time to save almost 50%! Visit our <a href=\"http://mylocalwebstop.com/shop/\">WordPress Store</a> for details.";
+				break;
+			case 7:
+				$mlw_advert_text = "Please consider taking our survey so we can improve this add-on. The survey only takes a few minutes. When you are ready, please <a href='http://mylocalwebstop.com/quiz-master-next-survey/'>take our survey</a>";
 				break;
 			default:
 				$mlw_advert_text = "Need support or features? Check out our Premium Support options! Visit our <a href=\"http://mylocalwebstop.com/shop/\">Plugin Add-On Store</a> for details!";
