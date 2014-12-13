0126d06a68c7ba1176eb3b8a20b29b09d96c29e7
Frank Corso <fpcorso@gmail.com>
2014-12-13 17:23:19 -0500

    Created System Info Function
    Created system information function and widget. Added system information function to support widget for sending the info with email.

--- includes/mlw_help.php
+++ includes/mlw_help.php
@@ -8,70 +8,202 @@ Copyright 2014, My Local Webstop (email : fpcorso@mylocalwebstop.com)
 
 function mlw_generate_help_page()
 {
+	///Creates the widgets
+	add_meta_box("wpss_mrts", 'Plugin Documentation', "qmn_documentation_meta_box_content", "meta_box_help");  
+	add_meta_box("wpss_mrts", 'Support', "qmn_support_meta_box_content", "meta_box_support");
+	add_meta_box("wpss_mrts", 'System Info', "qmn_system_meta_box_content", "meta_box_sys_info");
 	?>
-	<!-- css -->
-	<link type="text/css" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/themes/redmond/jquery-ui.css" rel="stylesheet" />
-	<!-- jquery scripts -->
-	<?php
-	wp_enqueue_script( 'jquery' );
-	wp_enqueue_script( 'jquery-ui-core' );
-	wp_enqueue_script( 'jquery-ui-dialog' );
-	wp_enqueue_script( 'jquery-ui-button' );
-	wp_enqueue_script( 'jquery-ui-accordion' );
-	wp_enqueue_script( 'jquery-effects-blind' );
-	wp_enqueue_script( 'jquery-effects-explode' );
-	?>
-	<!--<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.0/jquery.min.js"></script>
-	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>-->
-	<script type="text/javascript">
-		var $j = jQuery.noConflict();
-		// increase the default animation speed to exaggerate the effect
-		$j.fx.speeds._default = 1000;
-		$j(function() {
-			$j('#dialog').dialog({
-				autoOpen: false,
-				show: 'blind',
-				hide: 'explode',
-				buttons: {
-				Ok: function() {
-					$j(this).dialog('close');
-					}
-				}
-			});
-		
-			$j('#opener').click(function() {
-				$j('#dialog').dialog('open');
-				return false;
-		}	);
-		});
-		$j(function() {
-			$j("button").button();
-		});
-	</script>
-	<style>
-		label {
-	   		display: inline-block;
-	    	width: 5em;
-		}
-	</style>
-	<style type="text/css">
-		div.mlw_quiz_options input[type='text'] {
-			border-color:#000000;
-			color:#3300CC; 
-			cursor:hand;
-		}
-	</style>
 	<div class="wrap">
-	<div class='mlw_quiz_options'>
-	<h2>How-To<a id="opener" href="">(?)</a></h2>
-	Please visit our new <a href="http://mylocalwebstop.com/plugin-documentation/" style="color: blue;">Documentation</a> for using this plugin. It is a work in progress, but we will be deleting this page once it is finished.
-	<br />
-	<div id="dialog" title="Help">
-	<h3><b>Help</b></h3>
-	<p>This page contains numerous how-to's for using the plugin.</p>
-	</div>	
+	<h2>QMN Help</h2>
+	<?php echo mlw_qmn_show_adverts(); ?>
+	
+	<!--Display Widget Boxes-->
+	<div style="float:left; width:50%;" class="inner-sidebar1">
+		<?php do_meta_boxes('meta_box_help','advanced','');  ?>	
 	</div>
+	
+	<div style="float:left; width:50%;" class="inner-sidebar1">
+		<?php do_meta_boxes('meta_box_support','advanced','');  ?>	
+	</div>
+	
+	<div style="float:left; width:100%;" class="inner-sidebar1">
+		<?php do_meta_boxes('meta_box_sys_info','advanced','');  ?>	
+	</div>
+	
 	</div>	
 <?php
 }
+
+function qmn_documentation_meta_box_content()
+{
+	?>
+	<h3>Need help with the plugin? Try any of the following:</h3>
+	<ul>
+		<li>Visit our <a href="http://mylocalwebstop.com/plugin-documentation/">Documentation</a> for using this plugin</li>
+		<li>Fill out the form in the Support widget to send us an email</li>
+		<li>Fill out the form on our <a href="http://mylocalwebstop.com/contact-us/">Contact Us Page</a></li>
+		<li>Create a topic in the <a href="https://wordpress.org/support/plugin/quiz-master-next">WordPress Support Forums</a></li>
+	</ul>
+	<?php
+}
+
+function qmn_support_meta_box_content()
+{
+	$quiz_master_email_message = "";
+	$mlw_quiz_version = get_option('mlw_quiz_master_version');
+	if(isset($_POST["action"]))
+	{
+		$quiz_master_email_success = $_POST["action"];
+		$user_name = $_POST["username"];
+		$user_email = $_POST["email"];
+		$user_message = $_POST["message"];
+		$user_quiz_url = $_POST["quiz_url"];
+		$current_user = wp_get_current_user();
+		$mlw_site_info = qmn_get_system_info();
+		$mlw_site_info = str_replace("<br />", "\n", $mlw_site_info);
+		$mlw_site_info = str_replace("<h3>", "\n", $mlw_site_info);
+		$mlw_site_info = str_replace("</h3>", "\n", $mlw_site_info);
+		if ($quiz_master_email_success == 'update')
+		{
+			$mlw_message = "Message from ".$user_name." at ".$user_email." It says: \n \n ".$user_message."\n Version: ".$mlw_quiz_version."\n Quiz URL Provided: ".$user_quiz_url."\n User ".$current_user->display_name." from ".$current_user->user_email."\n Wordpress Info: ".$mlw_site_info;
+			wp_mail('fpcorso@mylocalwebstop.com' ,'Support From Quiz Master Next Plugin', $mlw_message);
+			$quiz_master_email_message = "**Message Sent**";
+		}
+	}
+	?>
+	<script>
+		function mlw_validateForm()
+		{
+			var x=document.forms['emailForm']['email'].value;
+			if (x==null || x=='')
+			{
+				document.getElementById('mlw_support_message').innerHTML = '**Email must be filled out!**';
+				return false;
+			};
+			var x=document.forms['emailForm']['username'].value;
+			if (x==null || x=='')
+			{
+				document.getElementById('mlw_support_message').innerHTML = '**Name must be filled out!**';
+				return false;
+			};
+			var x=document.forms['emailForm']['message'].value;
+			if (x==null || x=='')
+			{
+				document.getElementById('mlw_support_message').innerHTML = '**There must be a message to send!**';
+				return false;
+			};
+			var x=document.forms['emailForm']['email'].value;
+			var atpos=x.indexOf('@');
+			var dotpos=x.lastIndexOf('.');
+			if (atpos<1 || dotpos<atpos+2 || dotpos+2>=x.length)
+			{
+				document.getElementById('mlw_support_message').innerHTML = '**Not a valid e-mail address!**';
+				return false;
+			}
+		}
+	</script>
+	<div class='quiz_email_support'>
+	<form action="<?php echo $_SERVER['PHP_SELF']; ?>?page=mlw_help" method='post' name='emailForm' onsubmit='return mlw_validateForm()'>
+	<input type='hidden' name='action' value='update' />
+	<table>
+	<tr>
+	<td>If there is something you would like to suggest to add or even if you just want 
+	to let me know if you like the plugin or not, feel free to use the email form below.</td>
+	</tr>
+	<tr>
+	<td><span name='mlw_support_message' id='mlw_support_message' style="color: red;"><?php echo $quiz_master_email_message; ?></span></td>
+	</tr>
+	<tr>
+	<td align='left'><span style='font-weight:bold;';>Name (Required): </span></td>
+	</tr>
+	<tr>
+	<td><input type='text' name='username' value='' /></td>
+	</tr>
+	<tr>
+	<td align='left'><span style='font-weight:bold;';>Email (Required): </span></td>
+	</tr>
+	<tr>
+	<td><input type='text' name='email' value='' /></td>
+	</tr>
+	<tr>
+	<td align='left'><span style='font-weight:bold;';>URL To Quiz (Not Required): </span></td>
+	</tr>
+	<tr>
+	<td><input type='text' name='quiz_url' value='' /></td>
+	</tr>
+	<tr>
+	<td align='left'><span style='font-weight:bold;';>Message (Required): </span></td>
+	</tr>
+	<tr>
+	<td align='left'><TEXTAREA NAME="message" COLS=40 ROWS=6></TEXTAREA></td>
+	</tr>
+	<tr>
+	<td align='left'><input type='submit' class="button-primary" value='Send Email' /></td>
+	</tr>
+	<tr>
+	<td align='left'></td>
+	</tr>
+	</table>
+	</form>
+	<p>Disclaimer: In order to better assist you, this form will also send the system info from below with your message.</p>
+	</div>
+	<?php
+}
+
+function qmn_system_meta_box_content()
+{
+	echo qmn_get_system_info();
+}
+
+function qmn_get_system_info()
+{
+	global $wpdb;
+	$qmn_sys_info = "";
+	
+	$theme_data = wp_get_theme();
+	$theme      = $theme_data->Name . ' ' . $theme_data->Version;
+	
+	$qmn_sys_info .= "<h3>Site Information</h3><br />";
+	$qmn_sys_info .= "Site URL: ".site_url()."<br />";
+	$qmn_sys_info .= "Home URL: ".home_url()."<br />";
+	$qmn_sys_info .= "Multisite: ".( is_multisite() ? 'Yes' : 'No' )."<br />";
+	
+	$qmn_sys_info .= "<h3>WordPress Information</h3><br />";
+	$qmn_sys_info .= "Version: ".get_bloginfo( 'version' )."<br />";
+	$qmn_sys_info .= "Language: ".( defined( 'WPLANG' ) && WPLANG ? WPLANG : 'en_US' )."<br />";
+	$qmn_sys_info .= "Active Theme: ".$theme."<br />";
+	$qmn_sys_info .= "Debug Mode: ".( defined( 'WP_DEBUG' ) ? WP_DEBUG ? 'Enabled' : 'Disabled' : 'Not set' )."<br />";
+	$qmn_sys_info .= "Memory Limit: ".WP_MEMORY_LIMIT."<br />";
+	
+	$qmn_sys_info .= "<h3>Plugins Information</h3><br />";
+	$qmn_plugin_mu = get_mu_plugins();
+    	if( count( $qmn_plugin_mu > 0 ) ) {
+    		$qmn_sys_info .= "<h4>Must Use</h4><br />";
+	        foreach( $qmn_plugin_mu as $plugin => $plugin_data ) {
+	            $qmn_sys_info .= $plugin_data['Name'] . ': ' . $plugin_data['Version'] . "<br />";
+	        }
+    	}
+    	$qmn_sys_info .= "<h4>Active</h4><br />";
+	$plugins = get_plugins();
+	$active_plugins = get_option( 'active_plugins', array() );
+	foreach( $plugins as $plugin_path => $plugin ) {
+		if( !in_array( $plugin_path, $active_plugins ) )
+			continue;
+		$qmn_sys_info .= $plugin['Name'] . ': ' . $plugin['Version'] . "<br />";
+	}
+	$qmn_sys_info .= "<h4>Inactive</h4><br />";
+	foreach( $plugins as $plugin_path => $plugin ) {
+		if( in_array( $plugin_path, $active_plugins ) )
+			continue;
+		$qmn_sys_info .= $plugin['Name'] . ': ' . $plugin['Version'] . "<br />";
+	}
+	
+	$qmn_sys_info .= "<h3>Server Information</h3><br />";
+	$qmn_sys_info .= "PHP : ".PHP_VERSION."<br />";
+	$qmn_sys_info .= "MySQL : ".$wpdb->db_version()."<br />";
+	$qmn_sys_info .= "Webserver : ".$_SERVER['SERVER_SOFTWARE']."<br />";
+	
+	return $qmn_sys_info;
+}
+
 ?>
