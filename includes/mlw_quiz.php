1cfb39997da047506801f940a607eb887925f7a7
Frank Corso <fpcorso@gmail.com>
2014-12-26 18:49:38 -0500

    Moved Pagination Js To New Pagination JS File

--- includes/mlw_quiz.php
+++ includes/mlw_quiz.php
@@ -222,104 +222,13 @@ function mlw_quiz_shortcode($atts)
 			}
 			?>
 			<script type="text/javascript">
-				setTimeout(function(){
-				var $j = jQuery.noConflict();
-				$j( ".quiz_section" ).hide();
-				$j( ".quiz_section" ).append( "<br />" );
-				$j( ".mlw_qmn_quiz" ).append( "<a class=\"mlw_qmn_quiz_link mlw_previous\" href=\"javascript:prevSlide(<?php echo $mlw_quiz_options->pagination; ?>, 1);\"><?php echo $mlw_qmn_pagination_text[0]; ?></a>" );
-				$j( ".mlw_qmn_quiz" ).append( "<a class=\"mlw_qmn_quiz_link mlw_next\" href=\"javascript:nextSlide(<?php echo $mlw_quiz_options->pagination; ?>, 1);\"><?php echo $mlw_qmn_pagination_text[1]; ?></a>" );
-				window.mlw_quiz_slide = 0;
-				window.mlw_previous = 0;
-				window.mlw_quiz_total_slides = <?php echo $mlw_qmn_section_limit; ?>;
-				nextSlide(1, 0);
-				}, 100);
-				function nextSlide(mlw_pagination, mlw_goto_top)
-				{
-					jQuery( ".quiz_section" ).hide();
-					for (var i = 0; i < mlw_pagination; i++) 
-					{
-						if (i == 0 && window.mlw_previous == 1 && window.mlw_quiz_slide > 1)
-						{
-							window.mlw_quiz_slide = window.mlw_quiz_slide + mlw_pagination;
-						}
-						else
-						{
-							window.mlw_quiz_slide++;
-						}
-						if (window.mlw_quiz_slide < 1)
-						{
-							window.mlw_quiz_slide = 1;
-						}
-						if (window.mlw_quiz_slide == 1)
-					    {
-					    	jQuery( ".mlw_qmn_quiz_link.mlw_previous" ).hide();
-					    }
-					    if (window.mlw_quiz_slide > 1)
-					    {
-					    	jQuery( ".mlw_qmn_quiz_link.mlw_previous" ).show();
-					    }
-					    if (window.mlw_quiz_slide == window.mlw_quiz_total_slides)
-					    {
-					        jQuery( ".mlw_qmn_quiz_link.mlw_next" ).hide();
-					    } 
-					    if (window.mlw_quiz_slide < window.mlw_quiz_total_slides)
-					    {
-					        jQuery( ".mlw_qmn_quiz_link.mlw_next" ).show();
-					    } 
-					    jQuery( ".quiz_section.slide"+window.mlw_quiz_slide ).show();
-					}
-					window.mlw_previous = 0;
-					if (mlw_goto_top == 1)
-					{
-						window.location.hash = "mlw_does_not_exist";
-						window.location.hash = "mlw_top_of_quiz";
-					}
-				    
-				}
-				function prevSlide(mlw_pagination, mlw_goto_top)
-				{
-					jQuery( ".quiz_section" ).hide();
-					for (var i = 0; i < mlw_pagination; i++) 
-					{ 
-						if (i == 0 && window.mlw_previous == 0)
-						{
-							window.mlw_quiz_slide = window.mlw_quiz_slide - mlw_pagination;
-						}
-						else
-						{
-							window.mlw_quiz_slide--;
-						}
-						if (window.mlw_quiz_slide < 1)
-						{
-							window.mlw_quiz_slide = 1;
-						}
-						if (window.mlw_quiz_slide == 1)
-					    {
-					    	jQuery( ".mlw_qmn_quiz_link.mlw_previous" ).hide();
-					    }
-					    if (window.mlw_quiz_slide > 1)
-					    {
-					    	jQuery( ".mlw_qmn_quiz_link.mlw_previous" ).show();
-					    }
-					    if (window.mlw_quiz_slide == window.mlw_quiz_total_slides)
-					    {
-					        jQuery( ".mlw_qmn_quiz_link.mlw_next" ).hide();
-					    } 
-					    if (window.mlw_quiz_slide < window.mlw_quiz_total_slides)
-					    {
-					        jQuery( ".mlw_qmn_quiz_link.mlw_next" ).show();
-					    }
-					    jQuery( ".quiz_section.slide"+window.mlw_quiz_slide ).show();	
-					}
-					window.mlw_previous = 1;
-					if (mlw_goto_top == 1)
-					{
-						window.location.hash = "mlw_does_not_exist";
-						window.location.hash = "mlw_top_of_quiz";
-					}
-				}
+				var qmn_pagination = <?php echo $mlw_quiz_options->pagination; ?>;
+				var qmn_section_limit = <?php echo $mlw_qmn_section_limit; ?>;
+				var qmn_pagination_previous_text = <?php echo $mlw_qmn_pagination_text[0]; ?>;
+				var qmn_pagination_next_text = <?php echo $mlw_qmn_pagination_text[1]; ?>;
 			</script>
 			<?php
+			wp_enqueue_script( 'qmn_quiz_pagination', plugins_url( 'qmn_pagination.js' , __FILE__ ) );
 		}
 		if ($mlw_quiz_options->timer_limit != 0)
 		{
