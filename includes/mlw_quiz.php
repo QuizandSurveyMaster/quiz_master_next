0e8e783c3c0178c80b61103611310d94d71b635b
Frank Corso <fpcorso@gmail.com>
2014-12-26 19:00:04 -0500

    Moved Social Share JS To New Social Share JS File

--- includes/mlw_quiz.php
+++ includes/mlw_quiz.php
@@ -953,34 +953,8 @@ EOC;
 		
 		if ($mlw_quiz_options->social_media == 1)
 		{
-		?>
-			<script>
-			function mlw_qmn_share(network, mlw_qmn_social_text, mlw_qmn_title)
-			{
-				var sTop = window.screen.height/2-(218);
-                var sLeft = window.screen.width/2-(313);
-				var sqShareOptions = "height=400,width=580,toolbar=0,status=0,location=0,menubar=0,directories=0,scrollbars=0,top=" + sTop + ",left=" + sLeft;
-				var pageUrl = window.location.href;
-                var pageUrlEncoded = encodeURIComponent(pageUrl);
-				if (network == 'facebook')
-				{
-					var Url = "https://www.facebook.com/dialog/feed?"
-                    	+ "display=popup&"
-                        + "app_id=483815031724529&"
-                        + "link=" + pageUrlEncoded + "&"
-                        + "name=" + encodeURIComponent(mlw_qmn_social_text) + "&"
-						+ "description=  &"
-                        + "redirect_uri=http://www.mylocalwebstop.com/mlw_qmn_close.html";
-				}
-				if (network == 'twitter')
-				{
-					var Url = "https://twitter.com/intent/tweet?text=" + encodeURIComponent(mlw_qmn_social_text);
-				}
-                window.open(Url, "Share", sqShareOptions);
-                return false;
-            }
-			</script>
-			<?php
+			wp_enqueue_script( 'qmn_quiz_social_share', plugins_url( 'qmn_social_share.js' , __FILE__ ) );
+			
 			//Load Social Media Text
 			$qmn_social_media_text = "";
 			if (is_serialized($mlw_quiz_options->social_media_text) && is_array(@unserialize($mlw_quiz_options->social_media_text))) 
