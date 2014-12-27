2490b48c7c19c8254d3fab688c7af3b5db2fa3d8
Frank Corso <fpcorso@gmail.com>
2014-12-26 19:08:03 -0500

    Minor Update To Default Font

--- includes/mlw_quiz_creator.php
+++ includes/mlw_quiz_creator.php
@@ -203,6 +203,10 @@ class QMNQuizCreator
 					    display: inline-block;
 					    padding: 5px 5px 5px 5px;
    						margin: auto;
+				}
+				.mlw_qmn_question
+				{
+					font-weight: bold;
 				}";
 		$mlw_question_answer_default = "%QUESTION%<br /> Answer Provided: %USER_ANSWER%<br /> Correct Answer: %CORRECT_ANSWER%<br /> Comments Entered: %USER_COMMENTS%<br />";
 		$results = $wpdb->insert( 
