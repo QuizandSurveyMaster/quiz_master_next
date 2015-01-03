1b4295c72b9582a8fef094c83a1332bb15044b06
Frank Corso <fpcorso@gmail.com>
2015-01-03 13:07:49 -0500

    Added Functions For Adding Settings Tabs

--- includes/qmn_helper.php
+++ includes/qmn_helper.php
@@ -2,10 +2,12 @@
 class QMNPluginHelper
 {
 	public $addon_tabs = array();
+	public $settings_tabs = array();
 	
 	public function __construct()
 	{
-	
+		add_action('mlw_qmn_options_tab', array($this, 'get_settings_tabs'));
+		add_action('mlw_qmn_options_tab_content', array($this, 'get_settings_tabs_content'));
 	}
 	
 	public function register_addon_settings_tab($title, $function)
@@ -22,6 +24,35 @@ class QMNPluginHelper
 	public function get_addon_tabs()
 	{
 		return $this->addon_tabs;
+	}
+	
+	public function register_quiz_settings_tabs($title, $function)
+	{
+		$slug = strtolower(str_replace( " ", "-", $title));
+		$new_tab = array(
+			'title' => $title,
+			'function' => $function,
+			'slug' => $slug
+		);
+		$this->settings_tabs[] = $new_tab;
+	}
+	
+	public function get_settings_tabs()
+	{
+		foreach($this->settings_tab as $tab)
+		{
+			echo "<li><a href=\"".$tab["slug"]."\">".$tab["title"]."</a></li>";
+		}
+	}
+	
+	public function get_settings_tabs_content()
+	{
+		foreach($this->settings_tab as $tab)
+		{
+			echo "<div id=\"".$tab["slug"]."\" class=\"mlw_tab_content\">";
+			call_user_func($tab['function']);
+			echo "</div>";
+		}	
 	}
 }
 ?>
