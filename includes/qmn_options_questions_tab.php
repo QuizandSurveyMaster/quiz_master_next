ca96ff7fa8cef3488cee8bc8b51431ed7daa64a4
Frank Corso <fpcorso@gmail.com>
2015-01-02 23:16:37 -0500

    Added Action Calls

--- includes/qmn_options_questions_tab.php
+++ includes/qmn_options_questions_tab.php
@@ -762,4 +762,7 @@ function mlw_options_questions_tab_content()
 	</div>
 	<?php
 }
+
+add_action('mlw_qmn_options_tab', 'mlw_options_questions_tab');
+add_action('mlw_qmn_options_tab_content', 'mlw_options_questions_tab_content');
 ?>
