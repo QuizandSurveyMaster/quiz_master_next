91bbc8360dc8a7f89f208b5967300b2351f8ae00
Frank Corso <fpcorso@gmail.com>
2015-01-03 13:38:15 -0500

    Use New Plugin Helper To Register Tab

--- includes/qmn_options_tools_tab.php
+++ includes/qmn_options_tools_tab.php
@@ -1,8 +1,10 @@
 <?php
-function mlw_options_tools_tab()
+function qmn_settings_tools_tab()
 {
-	echo "<li><a href=\"#tabs-8\">Tools/Add-Ons</a></li>";
+	global $mlwQuizMasterNext;
+	$mlwQuizMasterNext->pluginHelper->register_quiz_settings_tabs("Tools", 'mlw_options_tools_tab_content');
 }
+add_action("plugins_loaded", 'qmn_settings_tools_tab');
 function mlw_options_tools_tab_content()
 {
 	global $wpdb;
@@ -77,6 +79,4 @@ function mlw_options_tools_tab_content()
 	</div>
 	<?php
 }
-add_action('mlw_qmn_options_tab', 'mlw_options_tools_tab');
-add_action('mlw_qmn_options_tab_content', 'mlw_options_tools_tab_content');
 ?>
