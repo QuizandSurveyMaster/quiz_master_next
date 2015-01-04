93b255dc204f874402e1446d2acb91c761543374
Frank Corso <fpcorso@gmail.com>
2015-01-04 14:48:21 -0500

    Added Gettext Functions

--- includes/qmn_options_style_tab.php
+++ includes/qmn_options_style_tab.php
@@ -2,7 +2,7 @@
 function qmn_settings_style_tab()
 {
 	global $mlwQuizMasterNext;
-	$mlwQuizMasterNext->pluginHelper->register_quiz_settings_tabs("Style", 'mlw_options_styling_tab_content');
+	$mlwQuizMasterNext->pluginHelper->register_quiz_settings_tabs(__("Style", 'quiz-master-next'), 'mlw_options_styling_tab_content');
 }
 add_action("plugins_loaded", 'qmn_settings_style_tab');
 
@@ -17,13 +17,13 @@ function mlw_options_styling_tab_content()
 		$mlw_qmn_style_id = intval($_POST["style_quiz_id"]);
 		$mlw_qmn_theme = $_POST["save_quiz_theme"];
 		$mlw_qmn_style = htmlspecialchars(stripslashes($_POST["quiz_css"]), ENT_QUOTES);
-		
+
 		//Save the new css
 		$mlw_save_stle_results = $wpdb->query( $wpdb->prepare( "UPDATE ".$wpdb->prefix."mlw_quizzes SET quiz_stye='%s', theme_selected='%s', last_activity='".date("Y-m-d H:i:s")."' WHERE quiz_id=%d", $mlw_qmn_style, $mlw_qmn_theme, $mlw_qmn_style_id ) );
 		if ($mlw_save_stle_results != false)
 		{
-			$mlwQuizMasterNext->alertManager->newAlert('The style has been saved successfully.', 'success');
-			
+			$mlwQuizMasterNext->alertManager->newAlert(__('The style has been saved successfully.', 'quiz-master-next'), 'success');
+
 			//Insert Action Into Audit Trail
 			global $current_user;
 			get_currentuserinfo();
@@ -31,14 +31,14 @@ function mlw_options_styling_tab_content()
 			$insert = "INSERT INTO " . $table_name .
 				"(trail_id, action_user, action, time) " .
 				"VALUES (NULL , '" . $current_user->display_name . "' , 'Styles Have Been Saved For Quiz Number ".$mlw_qmn_style_id."' , '" . date("h:i:s A m/d/Y") . "')";
-			$results = $wpdb->query( $insert );	
+			$results = $wpdb->query( $insert );
 		}
 		else
 		{
-			$mlwQuizMasterNext->alertManager->newAlert('There has been an error in this action. Please share this with the developer. Error Code: 0015.', 'error');
+			$mlwQuizMasterNext->alertManager->newAlert(printf(__('There has been an error in this action. Please share this with the developer. Error Code: %s', 'quiz-master-next'), '0015'), 'error');
 		}
 	}
-	
+
 	if (isset($_GET["quiz_id"]))
 	{
 		$table_name = $wpdb->prefix . "mlw_quizzes";
@@ -52,7 +52,7 @@ function mlw_options_styling_tab_content()
 				document.getElementById('save_quiz_theme').value = theme;
 				jQuery("div.mlw_qmn_themeBlockActive").toggleClass("mlw_qmn_themeBlockActive");
 				jQuery("#mlw_qmn_theme_block_"+theme).toggleClass("mlw_qmn_themeBlockActive");
-				
+
 			}
 		</script>
 		<?php
@@ -61,8 +61,8 @@ function mlw_options_styling_tab_content()
 			echo "<input type='hidden' name='style_quiz_id' value='".$quiz_id."' />";
 			echo "<input type='hidden' name='save_quiz_theme' id='save_quiz_theme' value='".$mlw_quiz_options->theme_selected."' />";
 		?>
-		<h3>Quiz Styles</h3>
-		<p>Choose your style:</p>
+		<h3><?php _e('Quiz Styles', 'quiz-master-next'); ?></h3>
+		<p><?php _e('Choose your style:', 'quiz-master-next'); ?></p>
 		<style>
 			div.mlw_qmn_themeBlock
 			{
@@ -84,33 +84,24 @@ function mlw_options_styling_tab_content()
 				background-color: yellow;
 			}
 		</style>
-		<div onclick="mlw_qmn_theme('default');" id="mlw_qmn_theme_block_default" class="mlw_qmn_themeBlock <?php if ($mlw_quiz_options->theme_selected == 'default') {echo 'mlw_qmn_themeBlockActive';} ?>">Custom</div>
+		<div onclick="mlw_qmn_theme('default');" id="mlw_qmn_theme_block_default" class="mlw_qmn_themeBlock <?php if ($mlw_quiz_options->theme_selected == 'default') {echo 'mlw_qmn_themeBlockActive';} ?>"><?php _e('Custom', 'quiz-master-next'); ?></div>
 		<?php do_action('mlw_qmn_quiz_themes'); ?>
 		<script>
-			mlw_qmn_theme('<?php echo $mlw_quiz_options->theme_selected; ?>');			
+			mlw_qmn_theme('<?php echo $mlw_quiz_options->theme_selected; ?>');
 		</script>
 		<br /><br />
-		<button id="save_styles_button" class="button" onclick="javascript: document.quiz_style_form.submit();">Save Quiz Style</button>
+		<button id="save_styles_button" class="button" onclick="javascript: document.quiz_style_form.submit();"><?php _e('Save Quiz Style', 'quiz-master-next'); ?></button>
 		<hr />
-		<h3>Custom Theme CSS</h3>
-		<p>Entire quiz is a div with class 'mlw_qmn_quiz'</p>
-		<p>Each page of the quiz is div with class 'quiz_section'</p>
-		<p>Message before quiz text is a span with class 'mlw_qmn_message_before'</p>
-		<p>The text for each question is wrapped in class 'mlw_qmn_question'</p>
-		<p>Each comment field for the questions is wrapped in class 'mlw_qmn_question_comment'</p>
-		<p>Label text for comment section is wrapped in class 'mlw_qmn_comment_section_text'</p>
-		<p>The message displayed at end of quiz is a span with class 'mlw_qmn_message_end'</p>
-		<p>Each button shown for pagination (i.e Next/Previous) is wrapped in class 'mlw_qmn_quiz_link'</p>
-		<p>Timer is wrapped in class 'mlw_qmn_timer'</p>
-		<p>Each horizontal multiple response is wrapped in a span with class 'mlw_horizontal_multiple'</p>
+		<h3><?php _e('Custom Theme CSS', 'quiz-master-next'); ?></h3>
+		<p><?php _e('For detailed help and guidance along with a list of different classes used in this plugin, please visit the following link:', 'quiz-master-next'); ?></p>
 		<button id="save_styles_button" class="button" onclick="javascript: document.quiz_style_form.submit();">Save Quiz Style</button>
 
 		<table class="form-table">
 			<tr>
 				<td width="66%"><textarea style="width: 100%; height: 100%;" id="quiz_css" name="quiz_css"><?php echo $mlw_quiz_options->quiz_stye; ?></textarea>
-				</td>	
+				</td>
 				<td width="30%">
-					<strong>Default:</strong><br />
+					<strong><?php _e('Default:', 'quiz-master-next'); ?></strong><br />
 					div.mlw_qmn_quiz input[type=radio],<br />
 					div.mlw_qmn_quiz input[type=submit],<br />
 					div.mlw_qmn_quiz label {<br />
@@ -155,7 +146,7 @@ function mlw_options_styling_tab_content()
 				</td>
 			</tr>
 		</table>
-		<button id="save_styles_button" class="button" onclick="javascript: document.quiz_style_form.submit();">Save Quiz Style</button>
+		<button id="save_styles_button" class="button" onclick="javascript: document.quiz_style_form.submit();"><?php _e('Save Quiz Style', 'quiz-master-next'); ?></button>
 		</form>
 	</div>
 	<?php
