995fa61505e4f115aeef9e4e9d4f3214c0ad6221
Frank Corso <fpcorso@gmail.com>
2015-02-08 23:44:40 -0500

    Added Multi-Delete Results Option

--- includes/qmn_results.php
+++ includes/qmn_results.php
@@ -33,6 +33,36 @@ function mlw_generate_quiz_results()
 		}
 	}
 
+	if (isset($_POST["bulk_delete"]) && $_POST["bulk_delete"] == "confirmation")
+	{
+		$results = $_POST["delete_results"];
+		if (is_array($results))
+		{
+			foreach($results as $result)
+			{
+				$wpdb->update(
+					$wpdb->prefix."mlw_results",
+					array(
+						'deleted' => 1,
+					),
+					array( 'result_id' => $result ),
+					array(
+						'%d'
+					),
+					array( '%d' )
+				);
+			}
+			//Insert Action Into Audit Trail
+			global $current_user;
+			get_currentuserinfo();
+			$table_name = $wpdb->prefix . "mlw_qm_audit_trail";
+			$insert = "INSERT INTO " . $table_name .
+				"(trail_id, action_user, action, time) " .
+				"VALUES (NULL , '" . $current_user->display_name . "' , 'Results Has Been Bulk Deleted' , '" . date("h:i:s A m/d/Y") . "')";
+			$results = $wpdb->query( $insert );
+		}
+	}
+
 	global $wpdb;
 	$mlw_qmn_table_limit = 30;
 	$mlw_qmn_results_count = $wpdb->get_var( "SELECT COUNT(result_id) FROM " . $wpdb->prefix . "mlw_results WHERE deleted='0'" );
@@ -114,112 +144,140 @@ function mlw_generate_quiz_results()
 		}
 	</style>
 	<div class="wrap">
-	<div class='mlw_quiz_options'>
-	<h2><?php _e('Quiz Results','quiz-master-next'); ?></h2>
-	<?php
-	$mlwQuizMasterNext->alertManager->showAlerts();
+		<div class='mlw_quiz_options'>
+			<h2><?php _e('Quiz Results','quiz-master-next'); ?></h2>
+			<?php
+			$mlwQuizMasterNext->alertManager->showAlerts();
+			?>
+			<div class="tablenav top">
+				<div class="alignleft actions bulkactions">
+					<a href="javascript: document.bulk_delete_form.submit();" class="button action">Bulk Delete</a>
+				</div>
+				<div class="tablenav-pages">
+					<span class="displaying-num"><?php echo sprintf(_n('One result', '%s results', $mlw_qmn_results_count, 'quiz-master-next'), number_format_i18n($mlw_qmn_results_count)); ?></span>
+					<span class="pagination-links">
+						<?php
+						$mlw_qmn_previous_page = 0;
+						$mlw_current_page = $mlw_qmn_result_page+1;
+						$mlw_total_pages = ceil($mlw_qmn_results_count/$mlw_qmn_table_limit);
+						if( $mlw_qmn_result_page > 0 )
+						{
+								$mlw_qmn_previous_page = $mlw_qmn_result_page - 2;
+								echo "<a class=\"prev-page\" title=\"Go to the previous page\" href=\"?page=mlw_quiz_results&&mlw_result_page=$mlw_qmn_previous_page\"><</a>";
+								echo "<span class=\"paging-input\">$mlw_current_page of $mlw_total_pages</span>";
+								if( $mlw_qmn_result_left > $mlw_qmn_table_limit )
+								{
+								echo "<a class=\"next-page\" title=\"Go to the next page\" href=\"?page=mlw_quiz_results&&mlw_result_page=$mlw_qmn_result_page\">></a>";
+								}
+							else
+							{
+								echo "<a class=\"next-page disabled\" title=\"Go to the next page\" href=\"?page=mlw_quiz_results&&mlw_result_page=$mlw_qmn_result_page\">></a>";
+								}
+						}
+						else if( $mlw_qmn_result_page == 0 )
+						{
+							if( $mlw_qmn_result_left > $mlw_qmn_table_limit )
+							{
+								echo "<a class=\"prev-page disabled\" title=\"Go to the previous page\" href=\"?page=mlw_quiz_results&&mlw_result_page=$mlw_qmn_previous_page\"><</a>";
+								echo "<span class=\"paging-input\">$mlw_current_page of $mlw_total_pages</span>";
+								echo "<a class=\"next-page\" title=\"Go to the next page\" href=\"?page=mlw_quiz_results&&mlw_result_page=$mlw_qmn_result_page\">></a>";
+							}
+						}
+						else if( $mlw_qmn_result_left < $mlw_qmn_table_limit )
+						{
+							$mlw_qmn_previous_page = $mlw_qmn_result_page - 2;
+							echo "<a class=\"prev-page\" title=\"Go to the previous page\" href=\"?page=mlw_quiz_results&&mlw_result_page=$mlw_qmn_previous_page\"><</a>";
+							echo "<span class=\"paging-input\">$mlw_current_page of $mlw_total_pages</span>";
+							echo "<a class=\"next-page disabled\" title=\"Go to the next page\" href=\"?page=mlw_quiz_results&&mlw_result_page=$mlw_qmn_result_page\">></a>";
+						}
+						?>
+					</span>
+					<br class="clear">
+				</div>
+			</div>
+			<form action="" method="post" name="bulk_delete_form">
+				<input type="hidden" name="bulk_delete" value="confirmation" />
+				<table class=widefat>
+					<thead>
+						<tr>
+							<th></th>
+							<th><?php _e('Actions','quiz-master-next'); ?></th>
+							<th><?php _e('Quiz Name','quiz-master-next'); ?></th>
+							<th><?php _e('Score','quiz-master-next'); ?></th>
+							<th><?php _e('Time To Complete','quiz-master-next'); ?></th>
+							<th><?php _e('Name','quiz-master-next'); ?></th>
+							<th><?php _e('Business','quiz-master-next'); ?></th>
+							<th><?php _e('Email','quiz-master-next'); ?></th>
+							<th><?php _e('Phone','quiz-master-next'); ?></th>
+							<th><?php _e('Time Taken','quiz-master-next'); ?></th>
+						</tr>
+					</thead>
+					<?php
+					$quotes_list = "";
+					$display = "";
+					$alternate = "";
+					foreach($mlw_quiz_data as $mlw_quiz_info) {
+						if($alternate) $alternate = "";
+						else $alternate = " class=\"alternate\"";
+						$mlw_complete_time = '';
+						$mlw_qmn_results_array = @unserialize($mlw_quiz_info->quiz_results);
+						if (is_array($mlw_qmn_results_array))
+						{
+							$mlw_complete_hours = floor($mlw_qmn_results_array[0] / 3600);
+							if ($mlw_complete_hours > 0)
+							{
+								$mlw_complete_time .= "$mlw_complete_hours hours ";
+							}
+							$mlw_complete_minutes = floor(($mlw_qmn_results_array[0] % 3600) / 60);
+							if ($mlw_complete_minutes > 0)
+							{
+								$mlw_complete_time .= "$mlw_complete_minutes minutes ";
+							}
+							$mlw_complete_seconds = $mlw_qmn_results_array[0] % 60;
+							$mlw_complete_time .=  "$mlw_complete_seconds seconds";
+						}
 
-	if( $mlw_qmn_result_page > 0 )
-	{
-	   	$mlw_qmn_previous_page = $mlw_qmn_result_page - 2;
-	   	echo "<a id=\"prev_page\" href=\"?page=mlw_quiz_results&&mlw_result_page=$mlw_qmn_previous_page\">".sprintf(__('Previous %s Results','quiz-master-next'),$mlw_qmn_table_limit)."</a>";
-	   	if( $mlw_qmn_result_left > $mlw_qmn_table_limit )
-	   	{
-			echo "<a id=\"next_page\" href=\"?page=mlw_quiz_results&&mlw_result_page=$mlw_qmn_result_page\">".sprintf(__('Next %s Results','quiz-master-next'),$mlw_qmn_table_limit)."</a>";
-	   	}
-	}
-	else if( $mlw_qmn_result_page == 0 )
-	{
-	   if( $mlw_qmn_result_left > $mlw_qmn_table_limit )
-	   {
-			echo "<a id=\"next_page\" href=\"?page=mlw_quiz_results&&mlw_result_page=$mlw_qmn_result_page\">".sprintf(__('Next %s Results','quiz-master-next'),$mlw_qmn_table_limit)."</a>";
-	   }
-	}
-	else if( $mlw_qmn_result_left < $mlw_qmn_table_limit )
-	{
-	   $mlw_qmn_previous_page = $mlw_qmn_result_page - 2;
-	   echo "<a id=\"prev_page\" href=\"?page=mlw_quiz_results&&mlw_result_page=$mlw_qmn_previous_page\">".sprintf(__('Previous %s Results','quiz-master-next'),$mlw_qmn_table_limit)."</a>";
-	}
-	?>
-	<table class=widefat>
-		<thead>
-			<tr>
-				<th><?php _e('Actions','quiz-master-next'); ?></th>
-				<th><?php _e('Quiz Name','quiz-master-next'); ?></th>
-				<th><?php _e('Score','quiz-master-next'); ?></th>
-				<th><?php _e('Time To Complete','quiz-master-next'); ?></th>
-				<th><?php _e('Name','quiz-master-next'); ?></th>
-				<th><?php _e('Business','quiz-master-next'); ?></th>
-				<th><?php _e('Email','quiz-master-next'); ?></th>
-				<th><?php _e('Phone','quiz-master-next'); ?></th>
-				<th><?php _e('Time Taken','quiz-master-next'); ?></th>
-			</tr>
-		</thead>
-		<?php
-		$quotes_list = "";
-		$display = "";
-		$alternate = "";
-		foreach($mlw_quiz_data as $mlw_quiz_info) {
-			if($alternate) $alternate = "";
-			else $alternate = " class=\"alternate\"";
-			$mlw_complete_time = '';
-			$mlw_qmn_results_array = @unserialize($mlw_quiz_info->quiz_results);
-			if (is_array($mlw_qmn_results_array))
-			{
-				$mlw_complete_hours = floor($mlw_qmn_results_array[0] / 3600);
-				if ($mlw_complete_hours > 0)
-				{
-					$mlw_complete_time .= "$mlw_complete_hours hours ";
-				}
-				$mlw_complete_minutes = floor(($mlw_qmn_results_array[0] % 3600) / 60);
-				if ($mlw_complete_minutes > 0)
-				{
-					$mlw_complete_time .= "$mlw_complete_minutes minutes ";
-				}
-				$mlw_complete_seconds = $mlw_qmn_results_array[0] % 60;
-				$mlw_complete_time .=  "$mlw_complete_seconds seconds";
-			}
+						$quotes_list .= "<tr{$alternate}>";
+						$quotes_list .= "<td><input type='checkbox' name='delete_results[]' value='".$mlw_quiz_info->result_id. "' /></td>";
+						$quotes_list .= "<td><span style='color:green;font-size:16px;'><a href='admin.php?page=mlw_quiz_result_details&&result_id=".$mlw_quiz_info->result_id."'>View</a>|<a onclick=\"deleteResults('".$mlw_quiz_info->result_id."','".esc_js($mlw_quiz_info->quiz_name)."')\" href='#'>Delete</a></span></td>";
+						$quotes_list .= "<td><span style='font-size:16px;'>" . $mlw_quiz_info->quiz_name . "</span></td>";
+						if ($mlw_quiz_info->quiz_system == 0)
+						{
+							$quotes_list .= "<td class='post-title column-title'><span style='font-size:16px;'>" . $mlw_quiz_info->correct ." out of ".$mlw_quiz_info->total." or ".$mlw_quiz_info->correct_score."%</span></td>";
+						}
+						if ($mlw_quiz_info->quiz_system == 1)
+						{
+							$quotes_list .= "<td><span style='font-size:16px;'>" . $mlw_quiz_info->point_score . " Points</span></td>";
+						}
+						if ($mlw_quiz_info->quiz_system == 2)
+						{
+							$quotes_list .= "<td><span style='font-size:16px;'>".__('Not Graded','quiz-master-next')."</span></td>";
+						}
+						$quotes_list .= "<td><span style='font-size:16px;'>" . $mlw_complete_time ."</span></td>";
+						$quotes_list .= "<td><span style='font-size:16px;'>" . $mlw_quiz_info->name ."</span></td>";
+						$quotes_list .= "<td><span style='font-size:16px;'>" . $mlw_quiz_info->business ."</span></td>";
+						$quotes_list .= "<td><span style='font-size:16px;'>" . $mlw_quiz_info->email ."</span></td>";
+						$quotes_list .= "<td><span style='font-size:16px;'>" . $mlw_quiz_info->phone ."</span></td>";
+						$quotes_list .= "<td><span style='font-size:16px;'>" . $mlw_quiz_info->time_taken ."</span></td>";
+						$quotes_list .= "</tr>";
+					}
+					$display .= "<tbody id=\"the-list\">{$quotes_list}</tbody>";
+					echo $display;
+					?>
+				</table>
+			</form>
+			<?php echo mlw_qmn_show_adverts(); ?>
 
-			$quotes_list .= "<tr{$alternate}>";
-			$quotes_list .= "<td><span style='color:green;font-size:16px;'><a href='admin.php?page=mlw_quiz_result_details&&result_id=".$mlw_quiz_info->result_id."'>View</a>|<a onclick=\"deleteResults('".$mlw_quiz_info->result_id."','".esc_js($mlw_quiz_info->quiz_name)."')\" href='#'>Delete</a></span></td>";
-			$quotes_list .= "<td><span style='font-size:16px;'>" . $mlw_quiz_info->quiz_name . "</span></td>";
-			if ($mlw_quiz_info->quiz_system == 0)
-			{
-				$quotes_list .= "<td class='post-title column-title'><span style='font-size:16px;'>" . $mlw_quiz_info->correct ." out of ".$mlw_quiz_info->total." or ".$mlw_quiz_info->correct_score."%</span></td>";
-			}
-			if ($mlw_quiz_info->quiz_system == 1)
-			{
-				$quotes_list .= "<td><span style='font-size:16px;'>" . $mlw_quiz_info->point_score . " Points</span></td>";
-			}
-			if ($mlw_quiz_info->quiz_system == 2)
-			{
-				$quotes_list .= "<td><span style='font-size:16px;'>".__('Not Graded','quiz-master-next')."</span></td>";
-			}
-			$quotes_list .= "<td><span style='font-size:16px;'>" . $mlw_complete_time ."</span></td>";
-			$quotes_list .= "<td><span style='font-size:16px;'>" . $mlw_quiz_info->name ."</span></td>";
-			$quotes_list .= "<td><span style='font-size:16px;'>" . $mlw_quiz_info->business ."</span></td>";
-			$quotes_list .= "<td><span style='font-size:16px;'>" . $mlw_quiz_info->email ."</span></td>";
-			$quotes_list .= "<td><span style='font-size:16px;'>" . $mlw_quiz_info->phone ."</span></td>";
-			$quotes_list .= "<td><span style='font-size:16px;'>" . $mlw_quiz_info->time_taken ."</span></td>";
-			$quotes_list .= "</tr>";
-		}
-		$display .= "<tbody id=\"the-list\">{$quotes_list}</tbody>";
-		echo $display;
-		?>
-	</table>
-
-	<?php echo mlw_qmn_show_adverts(); ?>
-
-	<div id="delete_dialog" title="Delete Results?" style="display:none;">
-		<h3><b><?php _e('Are you sure you want to delete these results?','quiz-master-next'); ?></b></h3>
-		<form action='' method='post'>
-			<input type='hidden' name='delete_results' value='confirmation' />
-			<input type='hidden' id='result_id' name='result_id' value='' />
-			<input type='hidden' id='delete_quiz_name' name='delete_quiz_name' value='' />
-			<p class='submit'><input type='submit' class='button-primary' value='<?php _e('Delete Results','quiz-master-next'); ?>' /></p>
-		</form>
-	</div>
-	</div>
+			<div id="delete_dialog" title="Delete Results?" style="display:none;">
+				<h3><b><?php _e('Are you sure you want to delete these results?','quiz-master-next'); ?></b></h3>
+				<form action='' method='post'>
+					<input type='hidden' name='delete_results' value='confirmation' />
+					<input type='hidden' id='result_id' name='result_id' value='' />
+					<input type='hidden' id='delete_quiz_name' name='delete_quiz_name' value='' />
+					<p class='submit'><input type='submit' class='button-primary' value='<?php _e('Delete Results','quiz-master-next'); ?>' /></p>
+				</form>
+			</div>
+		</div>
 	</div>
 <?php
 }
