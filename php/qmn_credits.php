c21b9f0cf9f496851fcc874d837f58f6f380949c
Frank Corso <fpcorso@mylocalwebstop.com>
2015-04-20 01:14:05 -0400

    Updates Translations and Changelog

--- php/qmn_credits.php
+++ php/qmn_credits.php
@@ -17,6 +17,12 @@ function mlw_generate_about_page()
 	wp_enqueue_style( 'qmn_admin_style', plugins_url( '../css/qmn_admin.css' , __FILE__ ) );
 	wp_enqueue_script('qmn_admin_js', plugins_url( '../js/admin.js' , __FILE__ ));
 	?>
+	<style>
+		div.mlw_qmn_icon_wrap
+		{
+			background: <?php echo 'url("'.plugins_url( 'images/quiz_icon.png' , __FILE__ ).'")'; ?> no-repeat;
+		}
+	</style>
 	<div class="wrap about-wrap">
 		<h1><?php _e('Welcome To Quiz Master Next', 'quiz-master-next'); ?></h1>
 		<div class="about-text"><?php _e('Thank you for updating!', 'quiz-master-next'); ?></div>
@@ -35,7 +41,7 @@ function mlw_generate_about_page()
 			<p style="text-align: center;">Continuing on with our process to slowly redesign the plugin, this update brings an updated stats page. We added new dynamic charts. There are many more items that we will be adding to this page over the next few updates.</p>
 			<br />
 			<h2 style="margin: 1.1em 0 .2em;font-size: 2.4em;font-weight: 300;line-height: 1.3;text-align: center;">Please Take Our Survey To Better Improve This Plugin</h2>
-			<p style="text-align: center;">When you have a moment, please take our survey for this plugin. By filling out the survey, you are helping us improve this plugin. Users who take the survey between now and March 31st, 2015 will be emailed a 25% off coupon for our WordPress Store. When you are ready, please <a href='http://mylocalwebstop.com/quiz-master-next-survey/'>take our survey</a>.</p>
+			<p style="text-align: center;">When you have a moment, please take our survey for this plugin. By filling out the survey, you are helping us improve this plugin. When you are ready, please <a href='http://mylocalwebstop.com/quiz-master-next-survey/'>take our survey</a>.</p>
 			<br />
 			<h2 style="margin: 1.1em 0 .2em;font-size: 2.4em;font-weight: 300;line-height: 1.3;text-align: center;">This Plugin Is Now Translation Ready!</h2>
 			<p style="text-align: center;">For those who wish to assist in translating, you can find the POT in the languages folder. If you do not know what that is, feel free to contact me and I will assist you with it.</p>
@@ -53,11 +59,11 @@ function mlw_generate_about_page()
 		<div id="mlw_quiz_changelog" style="display: none;">
 		<h3><?php echo $mlw_quiz_version; ?> (April 20, 2015)</h3>
 		<ul>
-			<li>* Redesigned Stats Page <a href='https://github.com/fpcorso/quiz_master_next/issues/177'>GitHub Issue #177</a></li>
-			<li>* Added Ability To Have Shortcodes In Questions <a href='https://github.com/fpcorso/quiz_master_next/issues/175'>GitHub Issue #175</a></li>
-			<li>* Added Editor To Questions <a href='https://github.com/fpcorso/quiz_master_next/issues/36'>GitHub Issue #36</a></li>
-			<li>* Added Ability To Edit Quiz Post <a href='https://github.com/fpcorso/quiz_master_next/issues/176'>GitHub Issue #176</a></li>
-			<li>* Added Ability To Search Questions <a href='https://github.com/fpcorso/quiz_master_next/issues/151'>GitHub Issue #151</a></li>
+			<li>* Redesigned Stats Page <a target="_blank" href='https://github.com/fpcorso/quiz_master_next/issues/177'>GitHub Issue #177</a></li>
+			<li>* Added Ability To Have Shortcodes In Questions <a target="_blank" href='https://github.com/fpcorso/quiz_master_next/issues/175'>GitHub Issue #175</a></li>
+			<li>* Added Editor To Questions <a target="_blank" href='https://github.com/fpcorso/quiz_master_next/issues/36'>GitHub Issue #36</a></li>
+			<li>* Added Ability To Edit Quiz Post <a target="_blank" href='https://github.com/fpcorso/quiz_master_next/issues/176'>GitHub Issue #176</a></li>
+			<li>* Added Ability To Search Questions <a target="_blank" href='https://github.com/fpcorso/quiz_master_next/issues/151'>GitHub Issue #151</a></li>
 			<li>* Design Changes To Questions Tab</li>
 			<li>* Minor Design Changes</li>
 			<li>* Minor Bug Fixes</li>
