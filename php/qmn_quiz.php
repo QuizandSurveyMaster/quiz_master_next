38f91810cf34a12293e6d6bfe0bd08abfa4b5245
Frank Corso <fpcorso@mylocalwebstop.com>
2015-06-20 14:23:21 -0400

    Fixes certificate site_url bug

--- php/qmn_quiz.php
+++ php/qmn_quiz.php
@@ -848,7 +848,7 @@ EOC;
 			$mlw_qmn_certificate_file.=<<<EOC
 \$pdf->Output('mlw_qmn_certificate.pdf','D');
 EOC;
-			$mlw_qmn_certificate_filename = str_replace(site_url()."/", '', plugin_dir_url( __FILE__ ))."certificates/mlw_qmn_quiz".date("YmdHis").$qmn_array_for_variables['timer'].".php";
+			$mlw_qmn_certificate_filename = str_replace(home_url()."/", '', plugin_dir_url( __FILE__ ))."certificates/mlw_qmn_quiz".date("YmdHis").$qmn_array_for_variables['timer'].".php";
 			file_put_contents($mlw_qmn_certificate_filename, $mlw_qmn_certificate_file);
 			$mlw_qmn_certificate_filename = plugin_dir_url( __FILE__ )."certificates/mlw_qmn_quiz".date("YmdHis").$qmn_array_for_variables['timer'].".php";
 			$mlw_certificate_link = "<a href='".$mlw_qmn_certificate_filename."' class='qmn_certificate_link'>Download Certificate</a>";
