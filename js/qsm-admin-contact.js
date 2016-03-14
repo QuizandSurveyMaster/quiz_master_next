969f0c604801e2d4b4ab3b0ed362ef6df024457b
Frank Corso <frank@mylocalwebstop.com>
2016-03-14 00:00:24 -0400

    Renames label key as label

--- js/qsm-admin-contact.js
+++ js/qsm-admin-contact.js
@@ -12,10 +12,38 @@ var QSMContact;
       });
     },
     addField : function( fieldArray ) {
-      
+      var required = '';
+      if ( fieldArray.required ) {
+        required = '*';
+      }
+      $( '.contact_form' ).append(
+        '<div class="contact-form-row">' +
+          '<div class="contact-form-group">' +
+            '<label>' +
+              fieldArray.label +
+              required +
+            '</label>' +
+            '<div class="contact-form-group-field">' +
+              '<input type="text">' +
+            '</div>' +
+          '</div>' +
+        '</div>'
+      );
+    },
+    newField : function() {
+      var fieldArray = {
+        label : 'Label',
+        type : 'text',
+        answers : [],
+        required : false
+      };
+      QSMContact.addField( fieldArray );
     }
   };
   $(function() {
-
+    QSMContact.load();
+    $( '.add-contact-field' ).on( 'click', function() {
+      QSMContact.newField();
+    });
   });
 }(jQuery));
