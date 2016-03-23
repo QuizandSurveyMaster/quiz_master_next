a1bdb22fc9ac9427dd1197f83d396cccd18a0bd7
Frank Corso <frank@mylocalwebstop.com>
2016-03-23 15:29:41 -0400

    Removes 25% from adverts

--- php/adverts-generate.php
+++ php/adverts-generate.php
@@ -9,33 +9,29 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 * @return $mlw_advert This variable is the main variable of the function and contains the advertisement content.
 * @since 4.4.0
 */
-function mlw_qmn_show_adverts()
-{
+function mlw_qmn_show_adverts() {
+
 	$mlw_advert = "";
 	$mlw_advert_text = "";
-	if ( get_option('mlw_advert_shows') == 'true' )
-	{
-		$mlw_random_int = rand(0, 4);
-		switch ($mlw_random_int) {
+	if ( 'true' == get_option( 'mlw_advert_shows' ) ) {
+
+		$mlw_random_int = rand( 0, 3 );
+		switch ( $mlw_random_int ) {
 			case 0:
-				// Support Advert 1
-				$mlw_advert_text = "Need support or features? Check out our Premium Support options! Visit our <a href=\"http://quizandsurveymaster.com/support-levels/?utm_source=qsm-plugin-ads&utm_medium=plugin&utm_content=support-advert-1&utm_campaign=qsm_plugin\">Quiz And Survey Master Support</a> for details!";
+				// Subscribe Newsletter 1
+				$mlw_advert_text = "Want a discount on your next addon purchase? Keep updated on our news, updated, and more by <a href=\"http://quizandsurveymaster.com/subscribe-to-our-newsletter/?utm_source=qsm-plugin-ads&utm_medium=plugin&utm_content=subscribe-newsletter-1&utm_campaign=qsm_plugin\">subscribing to our mailing list</a> and receive a 25% discount on your next purchase!";
 				break;
 			case 1:
-				// Subscribe Newsletter 1
-				$mlw_advert_text = "Want 25% off your next addon purchase? Keep updated on our news, updated, and more by <a href=\"http://quizandsurveymaster.com/subscribe-to-our-newsletter/?utm_source=qsm-plugin-ads&utm_medium=plugin&utm_content=subscribe-newsletter-1&utm_campaign=qsm_plugin\">subscribing to our mailing list</a> and receive a 25% discount on your next purchase!";
-				break;
-			case 2:
 				// Continued development 1
 				$mlw_advert_text = "Are you finding this plugin very beneficial? Please consider checking out our premium addons which help support continued development of this plugin. Visit our <a href=\"http://quizandsurveymaster.com/addons/?utm_source=qsm-plugin-ads&utm_medium=plugin&utm_content=continued-development-1&utm_campaign=qsm_plugin\">Addon Store</a> for details!";
 				break;
-			case 3:
+			case 2:
 				// Reporting and anaylsis 1
 				$mlw_advert_text = "Are you receiving a lot of responses to your quizzes and surveys? Consider our Reporting and Anaylsis addon which analyzes the data for you and allows you to filter the data as well as export it! <a href=\"http://quizandsurveymaster.com/downloads/results-analysis/?utm_source=qsm-plugin-ads&utm_medium=plugin&utm_content=reporting-analysis-1&utm_campaign=qsm_plugin\">Click here for more details!</a>";
 				break;
 			default:
-				// Support Advert 2
-				$mlw_advert_text = "Need support or features? Check out our Premium Support options! Visit our <a href=\"http://quizandsurveymaster.com/support-levels/?utm_source=qsm-plugin-ads&utm_medium=plugin&utm_content=support-advert-2&utm_campaign=qsm_plugin\">Quiz And Survey Master Support</a> for details!";
+				// Reporting and anaylsis 2
+				$mlw_advert_text = "Are you receiving a lot of responses to your quizzes and surveys? Consider our Reporting and Anaylsis addon which analyzes the data for you, graphs the data, allows you to filter the data, and export the data! <a href=\"http://quizandsurveymaster.com/downloads/results-analysis/?utm_source=qsm-plugin-ads&utm_medium=plugin&utm_content=reporting-analysis-2&utm_campaign=qsm_plugin\">Click here for more details!</a>";
 		}
 		$mlw_advert .= "
 			<style>
