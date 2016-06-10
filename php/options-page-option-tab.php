5a2795238620a5559a6a80f4138633cc1847498e
Frank Corso <frank@mylocalwebstop.com>
2016-06-09 22:40:18 -0400

    Updates function to call API section function

--- php/options-page-option-tab.php
+++ php/options-page-option-tab.php
@@ -7,12 +7,11 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 * @return void
 * @since 4.4.0
 */
-function qmn_settings_options_tab()
-{
+function qmn_settings_options_tab() {
 	global $mlwQuizMasterNext;
-	$mlwQuizMasterNext->pluginHelper->register_quiz_settings_tabs(__("Options", 'quiz-master-next'), 'mlw_options_option_tab_content');
+	$mlwQuizMasterNext->pluginHelper->register_quiz_settings_tabs( __( "Options", 'quiz-master-next' ), 'mlw_options_option_tab_content' );
 }
-add_action("plugins_loaded", 'qmn_settings_options_tab', 5);
+add_action( "plugins_loaded", 'qmn_settings_options_tab', 5 );
 
 /**
 * Adds the options content to the Quiz Settings page.
@@ -20,13 +19,11 @@ add_action("plugins_loaded", 'qmn_settings_options_tab', 5);
 * @return void
 * @since 4.4.0
 */
-function mlw_options_option_tab_content()
-{
+function mlw_options_option_tab_content() {
+	
 	global $wpdb;
 	global $mlwQuizMasterNext;
-	$quiz_id = $_GET["quiz_id"];
   $mlwQuizMasterNext->quizCreator->set_id( intval( $_GET["quiz_id"] ) );
-
-	QSMSETTINGSVAR->generate_settings_section( 'quiz_options' );
+	$mlwQuizMasterNext->pluginHelper->generate_settings_section( 'quiz_options' );
 }
 ?>
