5bafe327b5f96a87d57a031f394f29ae4b895437
Frank Corso <frank@mylocalwebstop.com>
2016-07-11 14:15:32 -0400

    Changes milestone for version

--- php/about-page.php
+++ php/about-page.php
@@ -48,7 +48,7 @@ function mlw_generate_about_page()
 		</div>
 		<div id="mlw_quiz_changelog" class="qmn_tab" style="display: none;">
 			<h2>Changelog</h2>
-			<?php QSM_Changelog_Generator::get_changelog_list( 'fpcorso/quiz_master_next', 18 ); ?>
+			<?php QSM_Changelog_Generator::get_changelog_list( 'fpcorso/quiz_master_next', 27 ); ?>
 		</div>
 		<div id="qmn_contributors" class="qmn_tab" style="display:none;">
 			<h2>GitHub Contributors</h2>
