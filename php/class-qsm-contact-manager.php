ed109c9df71ca5cf935ea56b8366bdbd6d88bf48
Frank Corso <frank@mylocalwebstop.com>
2017-02-11 19:59:27 -0500

    Adds prepare quiz call for settings

--- php/class-qsm-contact-manager.php
+++ php/class-qsm-contact-manager.php
@@ -184,6 +184,7 @@ class QSM_Contact_Manager {
     }
     global $mlwQuizMasterNext;
     $mlwQuizMasterNext->quizCreator->set_id( intval( $quiz_id ) );
+    $mlwQuizMasterNext->quiz_settings->prepare_quiz( intval( $quiz_id ) );
     return $mlwQuizMasterNext->pluginHelper->update_quiz_setting( "contact_form", serialize( $fields ) );
   }
 }
