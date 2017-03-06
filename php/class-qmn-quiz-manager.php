13edd873130b8740a420ce15af5d8e886e447146
Frank Corso <frank@mylocalwebstop.com>
2017-03-06 17:03:12 -0500

    Fixes submit button on quiz

--- php/class-qmn-quiz-manager.php
+++ php/class-qmn-quiz-manager.php
@@ -92,11 +92,11 @@ class QMNQuizManager {
 			// Check direct file first, then check templates folder in plugin, then check templates file in theme.
 			// If all fails, then load custom styling instead
 			if ( $registered_template && file_exists( $registered_template["path"] ) ) {
-				wp_enqueue_style( 'qmn_quiz_template', $registered_template["path"] );
+				wp_enqueue_style( 'qmn_quiz_template', $registered_template["path"], array(), $mlwQuizMasterNext->version );
 			} elseif ( $registered_template && file_exists( plugin_dir_path(__FILE__).'../templates/'.$registered_template["path"] ) ) {
-				wp_enqueue_style( 'qmn_quiz_template', plugins_url( '../templates/'.$registered_template["path"], __FILE__ ) );
+				wp_enqueue_style( 'qmn_quiz_template', plugins_url( '../templates/'.$registered_template["path"], __FILE__ ), array(), $mlwQuizMasterNext->version );
 			} elseif ( $registered_template && file_exists( get_stylesheet_directory_uri().'/templates/'.$registered_template["path"] ) ) {
-				wp_enqueue_style( 'qmn_quiz_template', get_stylesheet_directory_uri().'/templates/'.$registered_template["path"] );
+				wp_enqueue_style( 'qmn_quiz_template', get_stylesheet_directory_uri().'/templates/'.$registered_template["path"], array(), $mlwQuizMasterNext->version );
 			} else {
 				echo "<style type='text/css'>".$qmn_quiz_options->quiz_stye."</style>";
 			}
@@ -459,7 +459,7 @@ class QMNQuizManager {
 	    $section_display .= ob_get_contents();
     ob_end_clean();
 
-		$section_display .= "<input type='submit' class='qmn_btn' value='".esc_attr(htmlspecialchars_decode($qmn_quiz_options->submit_button_text, ENT_QUOTES))."' />";
+		$section_display .= "<input type='submit' class='qsm-btn qsm-submit-btn qmn_btn' value='".esc_attr(htmlspecialchars_decode($qmn_quiz_options->submit_button_text, ENT_QUOTES))."' />";
 		$section_display .= "</div>";
 
 		return $section_display;
