f1e4816adc3a7e7d49ab608b1ec2c5970f6aa0e5
Frank Corso <frank@frankcorso.me>
2018-05-10 16:20:08 -0400

    Improves progress bar design

--- js/qmn_quiz.js
+++ js/qmn_quiz.js
@@ -68,25 +68,21 @@ var QSM;
 						strokeWidth: 4,
 						easing: 'easeInOut',
 						duration: 1400,
-						color: '#FFEA82',
+						color: '#3498db',
 						trailColor: '#eee',
 						trailWidth: 1,
 						svgStyle: {width: '100%', height: '100%'},
-						// text: {
-						//   style: {
-						// 	// Text color.
-						// 	// Default: same as stroke color (options.color)
-						// 	color: '#999',
-						// 	position: 'absolute',
-						// 	right: '0',
-						// 	top: '30px',
-						// 	padding: 0,
-						// 	margin: 0,
-						// 	transform: null
-						//   },
-						//   autoStyleContainer: false
-						// },
-						from: {color: '#FFEA82'},
+						text: {
+						  style: {
+							// color: '#999',
+							position: 'absolute',
+							padding: 0,
+							margin: 0,
+							transform: null
+						  },
+						  autoStyleContainer: false
+						},
+						from: {color: '#3498db'},
 						to: {color: '#ED6A5A'},
 						step: (state, bar) => {
 						  bar.setText(Math.round(bar.value() * 100) + ' %');
