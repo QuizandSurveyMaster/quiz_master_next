64b6562a53c3e295ff412a96068017a25a026ab1
Frank Corso <frank@frankcorso.me>
2018-10-11 12:53:37 -0400

    Improves coding standards of settings page

--- php/admin/options-page-option-tab.php
+++ php/admin/options-page-option-tab.php
@@ -1,26 +1,27 @@
 <?php
-if ( ! defined( 'ABSPATH' ) ) exit;
-
-/**
-* Adds the Options tab to the Quiz Settings page.
-*
-* @return void
-* @since 4.4.0
-*/
-function qmn_settings_options_tab() {
-	global $mlwQuizMasterNext;
-	$mlwQuizMasterNext->pluginHelper->register_quiz_settings_tabs( __( "Options", 'quiz-master-next' ), 'mlw_options_option_tab_content' );
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
 }
-add_action( "plugins_loaded", 'qmn_settings_options_tab', 5 );
 
 /**
-* Adds the options content to the Quiz Settings page.
-*
-* @return void
-* @since 4.4.0
-*/
-function mlw_options_option_tab_content() {
+ * Adds the Options tab to the Quiz Settings page.
+ *
+ * @return void
+ * @since 6.1.0
+ */
+function qsm_settings_options_tab() {
+	global $mlwQuizMasterNext;
+	$mlwQuizMasterNext->pluginHelper->register_quiz_settings_tabs( __( 'Options', 'quiz-master-next' ), 'qsm_options_option_tab_content' );
+}
+add_action( 'plugins_loaded', 'qsm_settings_options_tab', 5 );
 
+/**
+ * Adds the options content to the Quiz Settings page.
+ *
+ * @return void
+ * @since 6.1.0
+ */
+function qsm_options_option_tab_content() {
 	global $wpdb;
 	global $mlwQuizMasterNext;
 	$mlwQuizMasterNext->pluginHelper->generate_settings_section( 'quiz_options' );
