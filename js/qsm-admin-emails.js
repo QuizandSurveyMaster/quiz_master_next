96bf217386e73f86189c521e9c75e8f11b606330
Frank Corso <frank@frankcorso.me>
2019-01-06 16:39:54 -0500

    Finishes switching results to emails in new email JS file

--- js/qsm-admin-emails.js
+++ js/qsm-admin-emails.js
@@ -8,22 +8,15 @@ var QSMAdminEmails;
 		saveEmails: function() {
 			var pages = [];
 			var page = {};
-			var redirect_value = '';
-			$( '.results-page' ).each( function() {
+			$( '.email' ).each( function() {
 				page = {
 					'conditions': [],
-					'page':  $( this ).find( '.results-page-template' ).val(),
-					'redirect': false,
 				};
-				redirect_value = $( this ).find( '.results-page-redirect' ).val();
-				if ( '' != redirect_value ) {
-					page.redirect = redirect_value;
-				}
-				$( this ).find( '.results-page-condition' ).each( function() {
+				$( this ).find( '.email-condition' ).each( function() {
 					page.conditions.push({
-						'criteria': $( this ).children( '.results-page-condition-criteria' ).val(),
-						'operator': $( this ).children( '.results-page-condition-operator' ).val(),
-						'value': $( this ).children( '.results-page-condition-value' ).val()
+						'criteria': $( this ).children( '.email-condition-criteria' ).val(),
+						'operator': $( this ).children( '.email-condition-operator' ).val(),
+						'value': $( this ).children( '.email-condition-value' ).val()
 					});
 				});
 				pages.push( page );
@@ -57,8 +50,8 @@ var QSMAdminEmails;
 				});
 		},
 		addCondition: function( $page, criteria, operator, value ) {
-			var template = wp.template( 'results-page-condition' );
-			$page.find( '.results-page-when-conditions' ).append( template({
+			var template = wp.template( 'email-condition' );
+			$page.find( '.email-when-conditions' ).append( template({
 				'criteria': criteria,
 				'operator': operator,
 				'value': value
@@ -68,11 +61,11 @@ var QSMAdminEmails;
 			QSMAdminEmails.addCondition( $page, 'score', 'equal', 0 );
 		},
 		addEmail: function( conditions, page, redirect ) {
-			var template = wp.template( 'results-page' );
-			$( '#results-pages' ).append( template( { page: page, redirect: redirect } ) );
+			var template = wp.template( 'email' );
+			$( '#emails' ).append( template( { page: page, redirect: redirect } ) );
 			conditions.forEach( function( condition, i, conditions) {
 				QSMAdminEmails.addCondition( 
-					$( '.results-page:last-child' ), 
+					$( '.email:last-child' ), 
 					condition.criteria,
 					condition.operator,
 					condition.value
@@ -100,18 +93,18 @@ var QSMAdminEmails;
 			event.preventDefault();
 			QSMAdminEmails.saveEmails();
 		});
-		$( '#results-pages' ).on( 'click', '.new-condition', function( event ) {
+		$( '#emails' ).on( 'click', '.new-condition', function( event ) {
 			event.preventDefault();
-			$page = $( this ).closest( '.results-page' );
+			$page = $( this ).closest( '.email' );
 			QSMAdminEmails.newCondition( $page );
 		});
-		$( '#results-pages' ).on( 'click', '.delete-page-button', function( event ) {
+		$( '#emails' ).on( 'click', '.delete-page-button', function( event ) {
 			event.preventDefault();
-			$( this ).closest( '.results-page' ).remove();
+			$( this ).closest( '.email' ).remove();
 		});
-		$( '#results-pages' ).on( 'click', '.delete-condition-button', function( event ) {
+		$( '#emails' ).on( 'click', '.delete-condition-button', function( event ) {
 			event.preventDefault();
-			$( this ).closest( '.results-page-condition' ).remove();
+			$( this ).closest( '.email-condition' ).remove();
 		});
 	});
 }(jQuery));
