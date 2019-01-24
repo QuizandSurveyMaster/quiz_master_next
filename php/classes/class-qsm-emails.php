c6a7ddd7eec4f39e12db356b56d39ec4d04f9d20
Frank Corso <frank@frankcorso.me>
2019-01-23 20:13:40 -0500

    Removes boolval since it is PHP5.5+

--- php/classes/class-qsm-emails.php
+++ php/classes/class-qsm-emails.php
@@ -191,7 +191,7 @@ class QSM_Emails {
 		$headers = array(
 			'From: ' . $from_name . ' <' . $from_email . '>',
 		);
-		if ( is_email( $user_email ) && boolval( $reply_to ) ) {
+		if ( is_email( $user_email ) && 'true' == $reply_to ) {
 			$name      = sanitize_text_field( $response_data['user_name'] );
 			$headers[] = 'Reply-To: ' . $name . ' <' . $user_email . '>';
 		}
