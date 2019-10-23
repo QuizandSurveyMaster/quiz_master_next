b54117a19d5a58537f625386f4b045f30810fb9e
Mark OBrien <mark.obrien@blueearth.net>
2019-10-23 14:34:57 -0500

    QSM-785 add filter to results array

--- php/classes/class-qmn-quiz-manager.php
+++ php/classes/class-qmn-quiz-manager.php
@@ -1050,7 +1050,7 @@ class QMNQuizManager {
                             $answer_points = 0;
 
                             // Send question to our grading function
-                            $results_array = $mlwQuizMasterNext->pluginHelper->display_review($question['question_type_new'], $question['question_id']);
+                            $results_array = apply_filters('qmn_results_array', $mlwQuizMasterNext->pluginHelper->display_review($question['question_type_new'], $question['question_id']));
 
                             // If question was graded correctly.
                             if (!isset($results_array["null_review"])) {
