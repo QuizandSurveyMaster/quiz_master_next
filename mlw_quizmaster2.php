6e4ce0e6d6889dac80ef15a6f0ff2c4bebaa14de
samitshah7493 <samitshah7493@gmail.com>
2020-04-29 14:05:48 +0530

    Added dashboard design - #2njbtx

--- mlw_quizmaster2.php
+++ mlw_quizmaster2.php
@@ -272,7 +272,9 @@ class MLWQuizMasterNext {
 	 */
 	public function setup_admin_menu() {
 		if ( function_exists( 'add_menu_page' ) ) {
-			add_menu_page( 'Quiz And Survey Master', __( 'Quizzes/Surveys', 'quiz-master-next' ), 'edit_posts', __FILE__, 'qsm_generate_quizzes_surveys_page', 'dashicons-feedback' );
+			add_menu_page( 'Quiz And Survey Master', __( 'Quiz And Survey Master', 'quiz-master-next' ), 'edit_posts', __FILE__, 'qsm_generate_dashboard_page', 'dashicons-feedback' );
+			add_submenu_page( __FILE__, __( 'Dashboard', 'quiz-master-next' ),  __( 'Dashboard', 'quiz-master-next' ), 'edit_posts', __FILE__, 'qsm_generate_dashboard_page' );
+			add_submenu_page( __FILE__, __( 'Quizzes/Surveys', 'quiz-master-next' ),  __( 'Quizzes/Surveys', 'quiz-master-next' ), 'edit_posts', 'mlw_quiz_list', 'qsm_generate_quizzes_surveys_page' );
 			add_submenu_page( NULL, __( 'Settings', 'quiz-master-next' ), __( 'Settings', 'quiz-master-next' ), 'edit_posts', 'mlw_quiz_options', 'qsm_generate_quiz_options' );
 			add_submenu_page( __FILE__, __( 'Results', 'quiz-master-next' ), __( 'Results', 'quiz-master-next' ), 'moderate_comments', 'mlw_quiz_results', 'qsm_generate_admin_results_page' );
 			add_submenu_page( NULL, __( 'Result Details', 'quiz-master-next' ), __( 'Result Details', 'quiz-master-next' ), 'moderate_comments', 'qsm_quiz_result_details', 'qsm_generate_result_details' );
