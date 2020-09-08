9e2be942e37e1c84239de9b5779f23ceaa4e1be7
samitshah7493 <samitshah7493@gmail.com>
2020-09-08 13:28:00 +0530

    Fixed certificate tab not showing issue

--- php/admin/admin-results-details-page.php
+++ php/admin/admin-results-details-page.php
@@ -15,7 +15,7 @@ function qsm_generate_result_details() {
         wp_enqueue_style( 'qsm_admin_style', plugins_url( '../../css/qsm-admin.css', __FILE__ ), array(), $mlwQuizMasterNext->version );        
         wp_enqueue_script( 'math_jax', '//cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.2/MathJax.js?config=TeX-MML-AM_CHTML' );
 	$active_tab = isset( $_GET[ 'tab' ] ) ? $_GET[ 'tab' ] : 'results';
-	$tab_array = $mlwQuizMasterNext->pluginHelper->get_results_tabs();
+	$tab_array = $mlwQuizMasterNext->pluginHelper->get_results_tabs();        
 	?>
         <style>
             .prettyprint {
@@ -24,7 +24,7 @@ function qsm_generate_result_details() {
         </style>
 	<div class="wrap">
                 <h2 style="display: none;"><?php _e('Quiz Results', 'quiz-master-next'); ?></h2>
-                <h2 class="nav-tab-wrapper" style="display: none;">
+                <h2 class="nav-tab-wrapper">
 			<?php
 			foreach( $tab_array as $tab ) {
 				$active_class = '';
