dba040b90461df31f3a3187d6e1955a7fdfed5a3
samitshah7493 <samitshah7493@gmail.com>
2020-12-31 13:16:43 +0530

    Added math jax support in result page

--- php/template-variables.php
+++ php/template-variables.php
@@ -231,7 +231,7 @@ function mlw_qmn_variable_social_share($content, $mlw_quiz_array) {
 	global $wpdb, $mlwQuizMasterNext;
 	$page_link = qsm_get_post_id_from_quiz_id($mlw_quiz_array['quiz_id']);
 	if (false !== strpos($content, '%FACEBOOK_SHARE%') || false !== strpos($content, '%TWITTER_SHARE%')) {
-		wp_enqueue_script( 'qmn_quiz_social_share', plugins_url( '../../js/qmn_social_share.js' , __FILE__ ) );
+		//wp_enqueue_script( 'qmn_quiz_social_share', plugins_url( '../../js/qmn_social_share.js' , __FILE__ ) );
 	}
 	if (false !== strpos($content, '%FACEBOOK_SHARE%')) {
 		$settings = (array) get_option('qmn-settings');
