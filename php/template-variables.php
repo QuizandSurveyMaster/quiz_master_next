4ddb7f6598244046c235489acfe390d14bcb1ba9
krunal patel <devloper.peacefulthemes@gmail.com>
2021-04-16 15:40:59 +0530

    Facebook Share not working

--- php/template-variables.php
+++ php/template-variables.php
@@ -238,10 +238,6 @@ function mlw_qmn_variable_social_share($content, $mlw_quiz_array) {
 		$facebook_app_id = '594986844960937';
 		if (isset($settings['facebook_app_id'])) {
 			$facebook_app_id = esc_js($settings['facebook_app_id']);
-            if($facebook_app_id == '483815031724529' || empty($facebook_app_id))
-            {
-                $facebook_app_id = '594986844960937';
-            }
 		}
 		$url = qsm_get_post_id_from_quiz_id($mlw_quiz_array['quiz_id']);
 		$page_link = $url . '?result_id=' . '%FB_RESULT_ID%';
