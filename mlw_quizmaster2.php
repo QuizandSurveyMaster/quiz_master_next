9212d5c8d5b4fcb4a314706828a4a09ede1e72b3
Aditya Jain <aditya@expresstech.io>
2021-04-28 17:03:08 +0530

    Release 7.1.16

--- mlw_quizmaster2.php
+++ mlw_quizmaster2.php
@@ -2,14 +2,14 @@
 /**
  * Plugin Name: Quiz And Survey Master
  * Description: Easily and quickly add quizzes and surveys to your website.
- * Version: 7.1.15
+ * Version: 7.1.16
  * Author: ExpressTech
  * Author URI: https://quizandsurveymaster.com/
  * Plugin URI: https://expresstech.io/
  * Text Domain: quiz-master-next
  *
  * @author QSM Team
- * @version 7.1.15
+ * @version 7.1.16
  * @package QSM
  */
 
@@ -37,7 +37,7 @@ class MLWQuizMasterNext {
 	 * @var string
 	 * @since 4.0.0
 	 */
-	public $version = '7.1.15';
+	public $version = '7.1.16';
 
 	/**
 	 * QSM Alert Manager Object
