b4017ac73a2b8025a395476ab10ccca2e1756c52
Aditya Jain <aditya@expresstech.io>
2021-06-09 18:29:01 +0530

    fixed typo

--- php/classes/class-qmn-quiz-manager.php
+++ php/classes/class-qmn-quiz-manager.php
@@ -1090,7 +1090,7 @@ class QMNQuizManager {
 		} else {
 			$end_section .= "<div class='qsm-auto-page-row quiz_section quiz_end empty_quiz_end' {$style}>";
 			$end_section .= "<input type='submit' class='qsm-btn qsm-submit-btn qmn_btn' value='" . esc_attr( htmlspecialchars_decode( $qmn_quiz_options->submit_button_text, ENT_QUOTES ) ) . "' />";
-			$end_display .= '</div>';
+			$end_section .= '</div>';
 		}
 
 		return $end_section;
