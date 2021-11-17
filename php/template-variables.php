ece1f3ff14c0ed6f28f3fb0c349a51af90c96ccc
Manoj Rao <manoj@expresstech.io>
2021-11-17 12:18:20 +0530

    code smells fixed

--- php/template-variables.php
+++ php/template-variables.php
@@ -1350,16 +1350,13 @@ function qmn_polar_display_on_resultspage( $id, $question, $answers, $answer ) {
 		$left_polar_title_style  = "style='font-weight:100;'";
 		$right_polar_title_style = "style='font-weight:900;'";
 	}elseif ( $answer['points'] == $mid_point ){
-		$left_polar_title_style  = "style='font-weight:100;'";
-		$right_polar_title_style = "style='font-weight:900;'";
+		$left_polar_title_style  = "style='font-weight:600;'";
+		$right_polar_title_style = "style='font-weight:600;'";
 	} elseif ( $answer['points'] < $mid_point ){
 		$left_polar_title_style  = "style='font-weight:400;'";
 		$right_polar_title_style = "style='font-weight:600;'";
 	}	elseif ( $answer['points'] > $mid_point ){
 		$left_polar_title_style  = "style='font-weight:600;'";
-		$right_polar_title_style = "style='font-weight:400;'";
-	} else {
-		$left_polar_title_style  = "style='font-weight:400;'";
 		$right_polar_title_style = "style='font-weight:400;'";
 	}
 	$new_question_title = $mlwQuizMasterNext->pluginHelper->get_question_setting( $id, 'question_title' );
