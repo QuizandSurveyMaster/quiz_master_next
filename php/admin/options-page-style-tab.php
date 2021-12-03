b6a9e91cc2efc941df52c379682e96dbc9168eaa
rashmiexpresstech <93512946+rashmiexpresstech@users.noreply.github.com>
2021-12-03 16:30:37 +0530

    bug fix sonar cloud

--- php/admin/options-page-style-tab.php
+++ php/admin/options-page-style-tab.php
@@ -187,8 +187,8 @@ function qsm_options_styling_tab_content() {
 </div>
 <form action='' method='post' name='quiz_style_form'>
 	<div id="legacy" class="quiz_style_tab_content" style="display: none;">
-		<p style="font-size: 18px;"><b><?php esc_html_e( 'Note: ', 'quiz-master-next' ); ?>
-			</b><?php esc_html_e( 'This option will be removed in future.', 'quiz-master-next' ); ?></p>
+		<p style="font-size: 18px;"><strong><?php esc_html_e( 'Note: ', 'quiz-master-next' ); ?>
+	</strong><?php esc_html_e( 'This option will be removed in future.', 'quiz-master-next' ); ?></p>
 		<input type='hidden' name='save_style_options' value='confirmation' />
 		<input type='hidden' name='style_quiz_id' value='<?php echo esc_attr( $quiz_id ); ?>' />
 		<input type='hidden' name='save_quiz_theme' id='save_quiz_theme'
