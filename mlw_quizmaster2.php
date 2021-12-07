9db712cfa83e679b0a316848fdbd1c2a725c2779
zubairraeen <raeenzubair1@gmail.com>
2021-12-07 17:28:20 +0530

    qsm update notice box html arranged

--- mlw_quizmaster2.php
+++ mlw_quizmaster2.php
@@ -522,7 +522,12 @@ class MLWQuizMasterNext {
 			?>
 			<div class="notice notice-info multiple-category-notice" style="display:none;">
 				<h3><?php esc_html_e( 'Database update required', 'quiz-master-next' ); ?></h3>
-				<p><?php esc_html_e( 'QSM has been updated! We need to upgrade your database so that you can enjoy the latest features.<br/>Please note that this action <b> can not be </b> rolled back. We recommend you to take a backup of your current site before proceeding.', 'quiz-master-next' ); ?></p>
+				<p>
+					<?php esc_html_e( 'QSM has been updated!', 'quiz-master-next' ); ?><br>
+					<?php esc_html_e( 'We need to upgrade your database so that you can enjoy the latest features.', 'quiz-master-next' ); ?><br>
+					<?php /* translators: %s: HTML tag */
+					echo sprintf( esc_html__( 'Please note that this action %s can not be %s rolled back. We recommend you to take a backup of your current site before proceeding.', 'quiz-master-next' ), '<b>', '</b>'); ?>
+				</p>
 				<p class="category-action">
 					<a href="#" class="button cancel-multiple-category"><?php esc_html_e( 'Cancel', 'quiz-master-next' ); ?></a>
 					&nbsp;&nbsp;&nbsp;<a href="#" class="button button-primary enable-multiple-category"><?php esc_html_e( 'Update Database', 'quiz-master-next' ); ?></a>
