b223de23b1126797036294f20ab7c043917c4eed
rashmiexpresstech <93512946+rashmiexpresstech@users.noreply.github.com>
2021-12-24 00:51:11 +0530

    Update qsm-admin.js

--- js/qsm-admin.js
+++ js/qsm-admin.js
@@ -2479,7 +2479,6 @@ if (jQuery('body').hasClass('admin_page_mlw_quiz_options')){
             // Adds event handlers for searching questions
             $('#question_search').on('keyup', function () {
                 $('.question').each(function () {
-                    var x = $(this).text().toLowerCase().indexOf($('#question_search').val().toLowerCase());
                     if ($(this).text().toLowerCase().indexOf($('#question_search').val().toLowerCase()) === -1) {
                         $(this).hide();
                     } else {
