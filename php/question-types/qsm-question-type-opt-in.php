31cc0a0fc01fe68fad786fa3ff4cb94890dd8834
unknown <dinesh@contriverz.com>
2022-05-16 14:38:08 +0530

    cloud issue

--- php/question-types/qsm-question-type-opt-in.php
+++ php/question-types/qsm-question-type-opt-in.php
@@ -22,9 +22,28 @@ function qmn_accept_display( $id, $question, $answers ) {
 	}
 	?>
 	<div class="qmn_accept_answers">
-		<input type="checkbox" id="mlwAcceptance" class="<?php echo esc_attr( $mlw_require_class ); ?>" />
+		<input type="checkbox" id="mlwAcceptance" name="<?php echo esc_attr( 'question' . $id ); ?>" class="<?php echo esc_attr( $mlw_require_class ); ?>" />
 		<label class="qsm-input-label" for="mlwAcceptance"><span class="qmn_accept_text"><?php echo wp_kses_post( do_shortcode( htmlspecialchars_decode( $question, ENT_QUOTES ) ) ); ?></span></label>
 	</div>
 	<?php
 	echo apply_filters( 'qmn_accept_display_front', '', $id, $question, $answers );
+}
+
+function qmn_opt_in_review( $id, $question, $answers ) {
+	if ( $_POST[ 'question' . $id ] == 'on' ) {
+		$user_compare_text = 'opted';
+	} else {
+		$user_compare_text = 'not-opted';
+	}
+	$current_question                  = new QSM_Question_Review_Choice( $id, $question, $answers );
+	$user_text_array                   = $current_question->get_user_answer();
+	$correct_text_array                = $current_question->get_correct_answer();
+	$return_array['user_answer']       = $user_text_array;
+	$return_array['correct_answer']    = $correct_text_array;
+	$return_array['user_text']         = ! empty( $user_text_array ) ? implode( '.', $user_text_array ) : '';
+	$return_array['correct_text']      = ! empty( $correct_text_array ) ? implode( '.', $correct_text_array ) : '';
+	$return_array['correct']           = 'opt-in';
+	$return_array['points']            = '';
+	$return_array['user_compare_text'] = $user_compare_text;
+	return apply_filters( 'qmn_opt_in_review', $return_array, $answers );
 }
