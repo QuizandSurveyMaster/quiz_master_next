a8bff8a4870e2b95c087d2661bd276c353184664
Mohammad Zubair Ali <zubair@expresstech.io>
2022-07-14 18:59:44 +0530

    fixed phpcs

--- php/admin/options-page-email-tab.php
+++ php/admin/options-page-email-tab.php
@@ -40,10 +40,10 @@ function qsm_options_emails_tab_content() {
 	);
 	wp_localize_script( 'qsm_admin_js', 'qsmEmailsObject', $js_data );
 	$quiz_options    = $mlwQuizMasterNext->pluginHelper->get_quiz_setting( 'quiz_options' );
-	if( isset( $quiz_options['send_email'] ) && 1 != $quiz_options['send_email'] ){
+	if ( isset( $quiz_options['send_email'] ) && 1 != $quiz_options['send_email'] ) {
 		?>
 		<div class="error notice-large notice-error">
-			<p><?php _e( 'Emails are turned off. Please update ');?> <a href="<?php echo admin_url('admin.php?page=mlw_quiz_options&quiz_id='.$quiz_id.'&tab=options');?>"> <?php _e('this setting', 'quiz-master-next');?></a><?php _e( ' for the emails to work properly.', 'quiz-master-next' ); ?></p>
+			<p><?php esc_html_e( 'Emails are turned off. Please update ', 'quiz-master-next');?> <a href="<?php echo admin_url('admin.php?page=mlw_quiz_options&quiz_id='.$quiz_id.'&tab=options');?>"> <?php esc_html_e('this setting', 'quiz-master-next');?></a><?php esc_html_e( ' for the emails to work properly.', 'quiz-master-next' ); ?></p>
 		</div>
 		<?php
 	}
