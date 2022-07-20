c34dbcf315c883cf59715f7b29f8ed5a83b8eaa7
Akshay Paghdar <akshay@justhyre.com>
2022-07-20 12:29:25 +0530

    Changed position for quiz options.

--- php/admin/functions.php
+++ php/admin/functions.php
@@ -707,34 +707,6 @@ function qsm_create_new_quiz_wizard() {
 								'option_name' => __( 'Grading System', 'quiz-master-next' ),
 								'value'       => $globalQuizsetting['system'],
 							),
-							'pagination'             => array(
-								'option_name' => __( 'Questions Per Page', 'quiz-master-next' ),
-								'value'       => $globalQuizsetting['pagination'],
-							),
-							'progress_bar'           => array(
-								'option_name' => __( 'Show Progress Bar', 'quiz-master-next' ),
-								'value'       => $globalQuizsetting['progress_bar'],
-							),
-							'timer_limit'            => array(
-								'option_name' => __( 'Time Limit (in Minute)', 'quiz-master-next' ),
-								'value'       => $globalQuizsetting['timer_limit'],
-							),
-							'enable_pagination_quiz' => array(
-								'option_name' => __( 'Show current page number', 'quiz-master-next' ),
-								'value'       => $globalQuizsetting['enable_pagination_quiz'],
-							),
-							'require_log_in'         => array(
-								'option_name' => __( 'Require User Login', 'quiz-master-next' ),
-								'value'       => $globalQuizsetting['require_log_in'],
-							),
-							'disable_scroll_next_previous_click' => array(
-								'option_name' => __( 'Disable scroll on next and previous button click?', 'quiz-master-next' ),
-								'value'       => $globalQuizsetting['disable_scroll_next_previous_click'],
-							),
-							'disable_first_page'     => array(
-								'option_name' => __( 'Disable first page on quiz', 'quiz-master-next' ),
-								'value'       => $globalQuizsetting['disable_first_page'],
-							),
 							'enable_contact_form'    => array(
 								'option_name' => __( 'Enable Contact Form', 'quiz-master-next' ),
 								'value'       => 0,
@@ -748,6 +720,18 @@ function qsm_create_new_quiz_wizard() {
 										'value' => 0,
 									),
 								),
+							),
+							'timer_limit'            => array(
+								'option_name' => __( 'Time Limit (in Minute)', 'quiz-master-next' ),
+								'value'       => $globalQuizsetting['timer_limit'],
+							),
+							'require_log_in'         => array(
+								'option_name' => __( 'Require User Login', 'quiz-master-next' ),
+								'value'       => $globalQuizsetting['require_log_in'],
+							),
+							'disable_first_page'     => array(
+								'option_name' => __( 'Disable first page on quiz', 'quiz-master-next' ),
+								'value'       => $globalQuizsetting['disable_first_page'],
 							),
 							'comment_section'        => array(
 								'option_name' => __( 'Enable Comment box', 'quiz-master-next' ),
