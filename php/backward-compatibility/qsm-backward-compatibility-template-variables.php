fd5646c70f9d0e511f8cf8913df22c0d358f6862
Vibina <vibina@expresstech.io>
2022-08-19 17:36:40 +0530

    removed the code for  testing  wporg-Image-answer-captions-not-displaying-on-the-results-page

--- php/backward-compatibility/qsm-backward-compatibility-template-variables.php
+++ php/backward-compatibility/qsm-backward-compatibility-template-variables.php
@@ -1,7 +1,6 @@
 <?php
 
 function qsm_bckcmp_tempvar_qa_text_qt_multi_choice_correct( $answers_from_db, $answers_from_response, $question_settings ) {
-    echo "<pre>";print_r($answers_from_db);exit;
     $question_with_answer_text = '';
     $new_array_user_answer = isset($answers_from_response['user_compare_text']) ? explode('=====', $answers_from_response['user_compare_text']) : array();
     foreach ( $answers_from_db as $single_answer ) {
@@ -81,7 +80,6 @@ function qsm_bckcmp_tempvar_qa_text_qt_single_choice_correct( $answers_from_db, 
 }
 
 function qsm_bckcmp_tempvar_qa_text_qt_multi_choice_points( $answers_from_db, $answers_from_response, $question_settings ) {
-    echo "<pre>";print_r($answers_from_db);exit;
     $question_with_answer_text = '';
     $user_selected_answer = htmlspecialchars_decode($answers_from_response[1], ENT_QUOTES);
     foreach ( $answers_from_db as $single_answer ) {
