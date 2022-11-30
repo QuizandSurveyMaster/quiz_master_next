76a941fb53698254a3f38d284fac51b6e07fb157
Mohammad Zubair Ali <zubair@expresstech.io>
2022-11-30 16:33:27 +0530

    update to 8.0.7

--- mlw_quizmaster2.php
+++ mlw_quizmaster2.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Quiz And Survey Master
  * Description: Easily and quickly add quizzes and surveys to your website.
- * Version: 8.0.6
+ * Version: 8.0.7
  * Author: ExpressTech
  * Author URI: https://quizandsurveymaster.com/
  * Plugin URI: https://expresstech.io/
@@ -43,7 +43,7 @@ class MLWQuizMasterNext {
 	 * @var string
 	 * @since 4.0.0
 	 */
-	public $version = '8.0.6';
+	public $version = '8.0.7';
 
 	/**
 	 * QSM Alert Manager Object
