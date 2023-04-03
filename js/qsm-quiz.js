a3cac84f3d8319619478b3f669cb0bc7fd711798
PranavAwasthi <pranavawasthi01@gmail.com>
2023-04-03 12:48:15 +0530

    Fixed button issue

--- js/qsm-quiz.js
+++ js/qsm-quiz.js
@@ -1123,7 +1123,7 @@ function qmnValidatePage(quiz_form_id) {
 
 // Show start quiz button if first page is visible
 function check_if_show_start_quiz_button(container, total_pages, page_number) {
-	if(jQuery('.quiz_begin').css('display') == 'block'){
+	if(container.find('.quiz_begin').css('display') == 'block'){
 		container.find(".mlw_custom_start").show();
 		container.find(".mlw_custom_next").hide();
 	}else{
