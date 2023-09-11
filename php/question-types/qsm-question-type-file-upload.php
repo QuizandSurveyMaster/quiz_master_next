8f5e2c3c0806be21181ea702bbbbef695945d4f1
Vibina <vibina@expresstech.io>
2023-09-11 19:40:45 +0530

    file upload function push

--- php/question-types/qsm-question-type-file-upload.php
+++ php/question-types/qsm-question-type-file-upload.php
@@ -26,6 +26,7 @@ function qmn_file_upload_display( $id, $question, $answers ) {
 		<span style="display: none;" class=" dashicons dashicons-trash remove-uploaded-file" class=""></span>
 		<span style="display: none;" class='mlw-file-upload-error-msg'></span>
 		<input class="mlw_file_upload_hidden_path" type="hidden" value="" />
+		<input class="mlw_file_upload_hidden_nonce" type="hidden" value="" />
 		<input class="mlw_file_upload_media_id" name="question<?php echo esc_attr( $id ); ?>" type="hidden" value="" />
 		<?php
 		echo apply_filters( 'qmn_file_upload_display_front', '', $id, $question, $answers );
