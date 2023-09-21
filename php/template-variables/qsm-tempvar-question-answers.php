00a01d44c3df3748e3b17a9964a515a2b447efbd
Vibina <vibina@expresstech.io>
2023-09-21 10:37:59 +0530

    validation for the rich type questions

--- php/template-variables/qsm-tempvar-question-answers.php
+++ php/template-variables/qsm-tempvar-question-answers.php
@@ -32,6 +32,8 @@ function qsm_tempvar_qa_text_qt_choice( $total_answers, $answers_from_response, 
 			}
 			$show_user_answer = '<img src="' . $mlwQuizMasterNext->pluginHelper->qsm_language_support( $single_answer[0], 'answer-' . $single_answer[0], 'QSM Answers' ) . '" style="' . esc_attr( $size_style ) . '" />';
 			$image_class      = 'qmn_image_option';
+		} elseif ( isset( $question_settings['answerEditor'] ) && 'rich' === $question_settings['answerEditor'] ) {
+			$show_user_answer = $mlwQuizMasterNext->pluginHelper->qsm_language_support( htmlspecialchars_decode( $single_answer[0], ENT_QUOTES ) );
 		} else {
 			$show_user_answer = $mlwQuizMasterNext->pluginHelper->qsm_language_support( $single_answer[0], 'answer-' . $single_answer[0], 'QSM Answers' );
 			$image_class      = '';
