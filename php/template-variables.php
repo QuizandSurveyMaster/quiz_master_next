0f0c6c1a9c7db01b2afd9d57367f7169e69fce22
Mohammad Zubair Ali <zubair@expresstech.io>
2023-12-04 13:32:21 +0530

    fixed sonar cloud issue

--- php/template-variables.php
+++ php/template-variables.php
@@ -1062,7 +1062,8 @@ function qsm_questions_answers_shortcode_to_text( $mlw_quiz_array, $qmn_question
 			$total_answers             = isset( $questions[ $answer['id'] ]['answers'] ) ? $questions[ $answer['id'] ]['answers'] : array();
 			if ( ! empty( $_POST['quiz_answer_random_ids'] ) ) {
 				$answers_random = array();
-				$quiz_answer_random_ids = maybe_unserialize( sanitize_text_field( wp_unslash( $_POST['quiz_answer_random_ids'] ) ) );
+				$quiz_answer_random_ids = sanitize_text_field( wp_unslash( $_POST['quiz_answer_random_ids'] ) );
+				$quiz_answer_random_ids = maybe_unserialize( $quiz_answer_random_ids );
 				if ( ! empty( $quiz_answer_random_ids[ $answer['id'] ] ) && is_array( $quiz_answer_random_ids[ $answer['id'] ] ) ) {
 					foreach ( $quiz_answer_random_ids[ $answer['id'] ] as $key ) {
 						$answers_random[ $key ] = $total_answers[ $key ];
