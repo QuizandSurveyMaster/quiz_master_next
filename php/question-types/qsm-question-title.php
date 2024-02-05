920203ffee1237bbede57606e199c9eac46ccc8a
PranavAwasthi <pranavawasthi01@gmail.com>
2024-02-05 13:47:58 +0530

    Fixed wpml issue with fill in the blank

--- php/question-types/qsm-question-title.php
+++ php/question-types/qsm-question-title.php
@@ -39,8 +39,7 @@ function qsm_question_title_func( $question, $question_type = '', $new_question_
 			<?php
 		}
 	}
-	$inline_drop_down = $mlwQuizMasterNext->pluginHelper->get_question_setting( $question_id, 'inline-drop-down' );
-	if ( ! empty( $question_title ) && 'fill_in_blank' !== $question_type && 0 !== $inline_drop_down ) {
+	if ( ! empty( $question_title ) && 'fill_in_blank' !== $question_type ) {
 		$question_title = $mlwQuizMasterNext->pluginHelper->qsm_language_support( htmlspecialchars_decode( html_entity_decode( $question_title, ENT_HTML5 ), ENT_QUOTES ), "question-description-{$question_id}", "QSM Questions" );
 	}
 	?>
