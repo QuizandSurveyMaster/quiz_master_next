c909bba5925c0ce06b8b35c94bd2fdc1cc8a6de9
etchirag <chirag@expresstech.io>
2024-09-24 16:07:33 +0530

    Minor changes to resolve other file change

--- php/classes/class-qmn-quiz-manager.php
+++ php/classes/class-qmn-quiz-manager.php
@@ -2417,8 +2417,8 @@ class QMNQuizManager {
 						$results_array = apply_filters( 'qmn_results_array', $results_array, $question );
 						// If question was graded correctly.
 						if ( ! isset( $results_array['null_review'] ) ) {
-							$points_earned += isset($results_array['points']) ? (float)$results_array['points'] : 0;
-							$answer_points += isset($results_array['points']) ? (float)$results_array['points'] : 0;
+							$points_earned += (float)$results_array['points'];
+							$answer_points += (float)$results_array['points'];
 							// If the user's answer was correct.
 							if ( isset( $results_array['correct'] ) && ( 'correct' == $results_array['correct'] ) ) {
 								$total_correct += 1;
