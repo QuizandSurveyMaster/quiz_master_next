7a9cef6d2eff779449d42ae118855ba5bcfe4a51
PranavAwasthi <pranavawasthi01@gmail.com>
2024-12-27 18:29:37 +0530

    removed print_r

--- php/classes/class-qmn-quiz-manager.php
+++ php/classes/class-qmn-quiz-manager.php
@@ -718,8 +718,6 @@ class QMNQuizManager {
 				wp_enqueue_script( 'math_jax', $this->mathjax_url, false, $this->mathjax_version, true );
 				wp_add_inline_script( 'math_jax', self::$default_MathJax_script, 'before' );
 				$quiz_result   = maybe_unserialize( $result_data['quiz_results'] );
-				echo '<pre>';
-				print_r( $quiz_result );
 				$response_data = array(
 					'quiz_id'                => $result_data['quiz_id'],
 					'quiz_name'              => $result_data['quiz_name'],
