b1fa4c59ee63bb30ea6e4fa6845082c33c8171a4
Mohammad Zubair Ali <zubair@expresstech.io>
2025-03-02 01:17:23 +0530

    run build

--- blocks/build/index.asset.php
+++ blocks/build/index.asset.php
@@ -1,4 +1,1 @@
-<?php return array(
-	'dependencies' => array( 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-editor', 'wp-element', 'wp-escape-html', 'wp-html-entities', 'wp-i18n', 'wp-notices' ),
-	'version'      => '15fecea714ebe89fbf2d',
-);
+<?php return array('dependencies' => array('wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-editor', 'wp-element', 'wp-escape-html', 'wp-html-entities', 'wp-i18n', 'wp-notices'), 'version' => 'ca8e53b08d26a202b462');
--- blocks/build/question/index.asset.php
+++ blocks/build/question/index.asset.php
@@ -1,4 +1,1 @@
-<?php return array(
-	'dependencies' => array( 'wp-api-fetch', 'wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-escape-html', 'wp-hooks', 'wp-i18n', 'wp-notices' ),
-	'version'      => '244621a3764d12292422',
-);
+<?php return array('dependencies' => array('wp-api-fetch', 'wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-escape-html', 'wp-hooks', 'wp-i18n', 'wp-notices'), 'version' => 'dd588e744de051517c95');
